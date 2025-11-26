chown2/OpenLR2

commit 402d43735d0423a4ec01d621822c0de36c78ed2b

refactor: replace WinApi threads with <thread>

LR2/LR2_ir.cpp
@@ -525,7 +525,9 @@ int NETWORK::Init() {
 	this->domain = "www.dream-pro.info";
 	this->timeout = 15000;
 	this->IRstatus = 0;
-	this->hHandle = NULL;
+	if (this->hHandle.joinable()) {
+		this->hHandle.join();
+	}
 	this->isOnline = 0;
 	this->IR_ID = 0;
 	this->rankingData.target_ID = 0;
@@ -684,12 +686,11 @@ int NETWORK::HTTPrequest() {
 void NETWORK::WaitAndInitRanking() {
 	GetTimeWrap();
 	this->waitForHandle = 1;
-	if (this->hHandle != NULL) {
-		WaitForSingleObject(this->hHandle, 5000);
+	if (hHandle.joinable()) {
+		hHandle.join();
 	}
 	this->waitForHandle = 0;
 	this->rankingData.Init();
-	return;
 }
 
 //4bc2f0
@@ -778,9 +779,6 @@ void IRsendScore(NETWORK *ir) {
 		ir->IRresultMessage = "サーバーとの接続に失敗しました";
 	}
 	ErrorLogAdd("送信終了\n");
-	ir->hHandle = NULL;
-
-	return;
 }
 
 //4bc970
@@ -841,7 +839,6 @@ NETWORK::NETWORK(){
 	domain = "www.dream-pro.info";
 	timeout = 15000;
 	IRstatus = 0;
-	hHandle = NULL;
 	rankUpdateDelayLevel = 0;
 	IR_ID = 0;
 }
@@ -976,13 +973,13 @@ int NETWORK::Login(int isDirectPlay) {
 int NETWORK::MakeIRsendScoreThread() {
 	GetTimeWrap();
 	this->waitForHandle = true;
-	if (this->hHandle) {
-		WaitForSingleObject(this->hHandle, 5000);
+	if (hHandle.joinable()) {
+		hHandle.join();
 	}
 	this->waitForHandle = false;
 	this->rankingData.Init();
 	this->IRstatus = 1;
-	this->hHandle = (HANDLE)_beginthread((void(*)(void*))IRsendScore, 0, this);
+	this->hHandle = std::jthread(IRsendScore, this);
 	return 0;
 }
 

LR2/Scene02_Songselect.cpp
@@ -59,7 +59,7 @@ int SetTarget(game *g) {
 
 
 //401fe0
-void ThreadProc_LoadBanner(void *param) {
+void ThreadProc_LoadBanner(void *param) { // TODO: take game&
 	game* g = (game*)param;
 
 	CSTR path;
@@ -1513,7 +1513,7 @@ void CheckNewSong(glb_dbgame *glb) {
 }
 
 //4181b0
-void ThreadProc_RankingAutoUpdate(void *param) {
+void ThreadProc_RankingAutoUpdate(void *param) { // TODO: take game&
 	game *g = (game*)param;
 
 	CSTR hash = g->sSelect.bmsList[g->sSelect.cur_song].hash;
@@ -1554,7 +1554,6 @@ void ThreadProc_RankingAutoUpdate(void *param) {
 		}
 
 		if (GetNowUnixtime() - GetFileUnixtime(path) < 86400 || g->config.network.autoupdate == 0) { //86400 is 24hours
-			g->net.hHandle = NULL;
 			g->sSelect.isRankingAutoUpdateThread = 0;
 			return;
 		}
@@ -1564,14 +1563,12 @@ void ThreadProc_RankingAutoUpdate(void *param) {
 
 	if (!g->config.network.autoupdate) {
 		if (!isIR2) g->net.IRstatus = 0;
-		g->net.hHandle = NULL;
 		g->sSelect.isRankingAutoUpdateThread = 0;
 		return;
 	}
 
 	if (g->net.rankUpdateDelayLevel >= 3) {
 		if (!isIR2) g->net.IRstatus = 5;
-		g->net.hHandle = NULL;
 		g->sSelect.isRankingAutoUpdateThread = 0;
 		return;
 	}
@@ -1582,7 +1579,6 @@ void ThreadProc_RankingAutoUpdate(void *param) {
 		Sleep(4);
 		if (g->net.waitForHandle || (isIR2 && (g->KeyInput.p1_buttonInput[4] == 2 || g->KeyInput.p2_buttonInput[4] == 2))) {
 			g->sSelect.isRankingAutoUpdateThread = 0;
-			g->net.hHandle = NULL;
 			ResetTimeLapse(177, &g->timer1);
 			g->net.IRstatus = isIR2 ? 2 : 0;
 			return;
@@ -1632,9 +1628,7 @@ void ThreadProc_RankingAutoUpdate(void *param) {
 	}
 
 	SetObjectStrings_SongSelect(g);
-	g->net.hHandle = NULL;
 	g->sSelect.isRankingAutoUpdateThread = 0;
-	return;
 }
 
 //419440
@@ -1646,7 +1640,6 @@ void LoadPreview(game *g) {
 	if (!IsFileExist(g->gameplay.previewBMSfilepath)) {
 		g->gameplay.isPreviewLoad = 0;
 		g->gameplay.previewStatus = 0;
-		g->gameplay.hThreadPreview = 0;
 		return;
 	}
 
@@ -1682,9 +1675,6 @@ void LoadPreview(game *g) {
 		g->gameplay.previewStatus = 0;
 		g->gameplay.isPreviewLoad = 0;
 	}
-
-	g->gameplay.hThreadPreview = 0;
-	return;
 }
 
 
@@ -1767,13 +1757,13 @@ int ProcS_Select(game *g) {
 	g->sSelect.levelsOfSong[4] = g->sSelect.bmsList[g->sSelect.cur_song].difficultyLevel[4];
 
 	if (g->sSelect.bmsList[g->sSelect.cur_song].isBanner && g->skstruct.reloadbanner == 1 && g->procSelecter == 2) {
-		g->hThreadBanner = _beginthread(ThreadProc_LoadBanner, 0, g);
+		g->hThreadBanner = std::jthread(ThreadProc_LoadBanner, g);
 	}
 	if (g->net.isOnline == 1 && g->procSelecter == 2) {
 		g->net.WaitAndInitRanking();
 		if (g->sSelect.bmsList[g->sSelect.cur_song].keymode >= 5 && (g->sSelect.bmsList[g->sSelect.cur_song].courseStageCount < 1 || g->sSelect.bmsList[g->sSelect.cur_song].courseIR)) {
 			g->net.IRstatus = 1;
-			g->net.hHandle = (HANDLE)_beginthread(ThreadProc_RankingAutoUpdate, 0, g);
+			g->net.hHandle = std::jthread(ThreadProc_RankingAutoUpdate, g);
 			SetObjectStrings_SongSelect(g);
 			return 1;
 		}
@@ -2662,13 +2652,13 @@ int ProcI_Select(game *g, sqlite3 *sql) {
 	}
 
 	if(g->config.select.preview == 1){
-		if (GetTimeLapse(11, &g->timer1) >= 500.0 && g->gameplay.previewStatus == 0 && g->sSelect.bmsList[g->sSelect.cur_song].keymode >= 5 && g->sSelect.bmsList[g->sSelect.cur_song].courseStageCount == 0 && g->gameplay.hThreadPreview == 0) {
+		if (GetTimeLapse(11, &g->timer1) >= 500.0 && g->gameplay.previewStatus == 0 && g->sSelect.bmsList[g->sSelect.cur_song].keymode >= 5 && g->sSelect.bmsList[g->sSelect.cur_song].courseStageCount == 0 && !g->gameplay.hThreadPreview.joinable()) {
 			g->gameplay.flag_closingPhase = 1;
 			g->gameplay.isPreviewLoad = 0;
 			g->gameplay.previewStatus = 1;
 			g->gameplay.previewBMShash = g->sSelect.bmsList[g->sSelect.cur_song].hash;
 			g->gameplay.previewBMSfilepath = g->sSelect.bmsList[g->sSelect.cur_song].filepath;
-			g->gameplay.hThreadPreview = (HANDLE)_beginthread((void(*)(void*))LoadPreview, 0, g);
+			g->gameplay.hThreadPreview = std::jthread(LoadPreview, g);
 		}
 		else if (g->gameplay.previewStatus) {
 			if (g->gameplay.previewBMShash.isDiff(g->sSelect.bmsList[g->sSelect.cur_song].hash)) {

LR2/structure.h
@@ -4,6 +4,7 @@
 #include <vfw.h>
 #include <vector>
 #include <array>
+#include <thread>
 #include "FMOD/fmod.h"
 #include "strclass.h"
 typedef unsigned char   undefined;
@@ -1407,7 +1408,7 @@ struct gameplay {
 	int fadeoutBGAend[10];
 	int bgaMixer[10];
 	char isPreviewLoad;
-	HANDLE hThreadPreview;
+	std::jthread hThreadPreview;
 	int previewStatus; /* 1:start 2:loaded */
 	CSTR previewBMShash;
 	CSTR previewBMSfilepath;
@@ -1482,7 +1483,7 @@ struct NETWORK {
 	int loginResult;
 	struct RANKING rankingData;
 	struct MYRANKING myRanking;
-	HANDLE hHandle;
+	std::jthread hHandle;
 	int IRstatus; /* 0:notIR 1:loading 2:loaded -1:playerNotExist -3:connection_fail -2:bansong 3:waitUpadate 4:connection 5:IRbusy */
 	CSTR IRresultMessage;
 
@@ -1534,7 +1535,7 @@ struct game {
 	int po4MainMenuCursor;
 	int procSelecter; /* 2:select 3:deciide 4:play 5:result 6:keyconfig 7:skinselect */
 	int procPhase;
-	int hThreadBanner;
+	std::jthread hThreadBanner;
 	struct gameplay gameplay;
 	CRITICAL_SECTION criticalSection;
 	char is_clicked_screenModeChange;

LR2/Main.cpp
@@ -163,9 +163,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 			else if (tStr2.left(2).isSame("-a")) {
 				gs.cmd_auto = '\x01';
 			}
-			else if (tStr2.left(2).isSame("-n")) {
+			/*else if (tStr2.left(2).isSame("-n")) {
 				atol(tStr1.right(tStr1.length() - 2));
-			}
+			}*/
 		}
 		gs.config.system.thread = 0;
 		cstrSprintf(&pathScoreDB, "LR2files/Database/Score/%s.db", gs.config.player.id);
@@ -440,7 +440,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 			gs.gameplay.previewStatus = 0;
 			InitializeCriticalSection(&gs.gameplay.criticalSection);
 			InitializeCriticalSection(&gs.criticalSection);
-			gs.gameplay.hThreadPreview = 0;
+			if (gs.gameplay.hThreadPreview.joinable()) {
+				gs.gameplay.hThreadPreview.join();
+			}
 			gs.gameplay.courseType = -1;
 			gs.gameplay.courseStageNow = 0;
 			gs.gameplay.timetick = GetTimeWrap();
@@ -448,7 +450,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 			InitSkin(&gs.skstruct, 0, 0);
 			gs.skstruct.fontname.assign(&gs.config.skin.fontname);
 			gs.skstruct.unused_disableimagefont = gs.config.skin.disableimagefont;
-			gs.hThreadBanner = 0;
+			if(gs.hThreadBanner.joinable()) gs.hThreadBanner.join();
 			for (int i = 0; i < 6480; i++) gs.gameplay.keysound->load = 0;
 			for (int i = 0; i < 200; i++) gs.skstruct2.caption[i].fillzero();
 			for (int i = 0; i < 10; i++) gs.skstruct2.helpfilePath[i].fillzero();

LR2/Scene04_Play.cpp
@@ -1625,7 +1625,7 @@ void ProcGameThread(game *g) {
 	}
 
 	if (g->gameplay.flag_retry == 0 && g->config.system.isablebmsthread == 0) {
-		_beginthread((void(*)(void*))ProcLoadBmsResource, 0, g);
+		std::jthread(ProcLoadBmsResource, g).detach();
 	}
 	else {
 		g->gameplay.bmsResourceLoaded = 1;
@@ -1732,7 +1732,9 @@ int ProcS_Play(game *g, sqlite3* sql) {
 
 	int iTemp;
 
-	if (g->gameplay.hThreadPreview) WaitForSingleObject(g->gameplay.hThreadPreview, 5000);
+	if (g->gameplay.hThreadPreview.joinable()) {
+		g->gameplay.hThreadPreview.join();
+	}
 	g->gameplay.flag_closingPhase = 0;
 	
 	CSTR gData, gName;
@@ -1947,8 +1949,7 @@ int ProcS_Play(game *g, sqlite3* sql) {
 	}
 
 	SetObjectString(1, g->gameplay.targetScore.name, g->txtStruct.objectStr);
-	HANDLE hG = (HANDLE)_beginthread((void(*)(void*))ProcGameThread,0,g);
-	SetThreadPriority(hG, -1);
+	std::jthread(ProcGameThread, g).detach(); // removed SetThreadPriority(hG, -1);
 	return 1;
 }
 

LR2/Scene05_Result.cpp
@@ -211,8 +211,8 @@ int ProcI_Result(game *g) {
 			SetObjectString(20, g->net.IRresultMessage, g->txtStruct.objectStr);
 		}
 		else {
-			if (GetTimeLapse(151, &g->timer1) <= g->skstruct.startinput_rank || GetTimeLapse(152, &g->timer1) != -1.0 || (g->net.isOnline && (g->net.isOnline != 1 || g->net.hHandle))) {
-				if (GetTimeLapse(151, &g->timer1) > g->skstruct.startinput_rank && GetTimeLapse(152, &g->timer1) == -1.0 && g->net.isOnline == 1 && g->net.hHandle) {
+			if (GetTimeLapse(151, &g->timer1) <= g->skstruct.startinput_rank || GetTimeLapse(152, &g->timer1) != -1.0 || (g->net.isOnline && (g->net.isOnline != 1 || g->net.hHandle.joinable()))) {
+				if (GetTimeLapse(151, &g->timer1) > g->skstruct.startinput_rank && GetTimeLapse(152, &g->timer1) == -1.0 && g->net.isOnline == 1 && g->net.hHandle.joinable()) {
 					fWaitHiScoreUpdateInput = 1;
 				}
 				else if (GetTimeLapse(152, &g->timer1) > g->skstruct.startinput_update && GetTimeLapse(2, &g->timer1) == -1.0) {

commit 47e550bec97c5dabb666391406a25449f97c5a18

fix: detach IR thread
Fixes result getting stuck when sending scores.

Fixes: 402d43735d04 ("refactor: replace WinApi threads with <thread>")

LR2/LR2_ir.cpp
@@ -730,23 +730,25 @@ int OpenWebRanking(CSTR songmd5){
 #endif
 }
 
-//4bc6b0 //
-void IRsendScore(NETWORK *ir) {
-	int httpResponse;
-	CSTR sData;
-
+static void ThreadProc_IRsendScore(NETWORK *ir) {
 	ErrorLogAdd("LR2IRにスコアを送信します。\n");
-	cstrSprintf(&sData, "%s%s%d%d", ir->IR_passMD5.body, ir->myRanking.songMD5.body,ir->myRanking.exscore, ir->myRanking.clear);
-	sData = MD5str(sData);
+	CSTR scorehash;
+	cstrSprintf(&scorehash, "%s%s%d%d", ir->IR_passMD5.body, ir->myRanking.songMD5.body,ir->myRanking.exscore, ir->myRanking.clear);
+	scorehash = MD5str(scorehash);
 
 	cstrSprintf(&ir->param, "songmd5=%s&id=%d&passmd5=%s&title=%s&genre=%s&artist=%s&maxbpm=%d&minbpm=%d&&playlevel=%d&clear=%d&exscore=%d&pg=%d&gr=%d&gd=%d&bd=%d&pr=%d&maxcombo=%d&playcount=%d&clearcount=%d&rate=%d&minbp=%d&totalnotes=%d&opt_history=%d&opt_this=%d&line=%d&judge=%d&inputtype=%d&ghost=%s&rseed=%d&clear_db=%d&clear_ex=%d&clear_sd=%d&scorehash=%s"
 		, ir->myRanking.songMD5.body, ir->IR_ID, ir->IR_passMD5.body, UrlEncode(ir->myRanking.title).body, UrlEncode(ir->myRanking.genre).body, UrlEncode(ir->myRanking.artist).body,
 		ir->myRanking.maxbpm, ir->myRanking.minbpm, ir->myRanking.playlevel, ir->myRanking.clear, ir->myRanking.exscore, ir->myRanking.pg, ir->myRanking.gr, ir->myRanking.gd, ir->myRanking.bd, ir->myRanking.pr, ir->myRanking.maxcombo,
 		ir->myRanking.playcount, ir->myRanking.clearcount, ir->myRanking.rate, ir->myRanking.minbp, ir->myRanking.totalnotes, ir->myRanking.opt_history, ir->myRanking.opt_this, ir->myRanking.line, ir->myRanking.judge,
-		ir->myRanking.inputtype, ir->myRanking.ghost.body, ir->myRanking.rseed,	ir->myRanking.clear_db, ir->myRanking.clear_ex, ir->myRanking.clear_sd, UrlEncode(sData).body);
+		ir->myRanking.inputtype, ir->myRanking.ghost.body, ir->myRanking.rseed,	ir->myRanking.clear_db, ir->myRanking.clear_ex, ir->myRanking.clear_sd, UrlEncode(scorehash).body);
 
 	ir->target_URL = "http://www.dream-pro.info/~lavalse/LR2IR/2/score.cgi";
-	httpResponse = 0; //DEBUG: do not send IR before test is done enough. ir->HTTPrequest();
+	int httpResponse;
+	if constexpr (true) { // DEBUG: do not send IR before test is done enough.
+		httpResponse = 0;
+	} else {
+		httpResponse = ir->HTTPrequest();
+	}
 	if (httpResponse == 1) {
 		ir->IRresultMessage = "スコアを送信しました";
 		ir->GetRanking(ir->myRanking.songMD5, 1);
@@ -755,6 +757,8 @@ void IRsendScore(NETWORK *ir) {
 		ir->IRresultMessage = "サーバーとの接続に失敗しました";
 	}
 	ErrorLogAdd("送信終了\n");
+
+	ir->hHandle.detach(); // Detach ourselves TODO: refactor surrounding code to avoid this
 }
 
 //4bc970
@@ -954,7 +958,7 @@ int NETWORK::MakeIRsendScoreThread() {
 	this->waitForHandle = false;
 	this->rankingData.Init();
 	this->IRstatus = 1;
-	this->hHandle = std::jthread(IRsendScore, this);
+	this->hHandle = std::jthread(ThreadProc_IRsendScore, this);
 	return 0;
 }
 

LR2/LR2_ir.h
@@ -5,6 +5,5 @@ int CheckRivaldataNew(int rivalID);
 int ParseRivalData(long ID);
 CSTR UrlEncode(CSTR in);
 int OpenWebRanking(CSTR songmd5);
-void IRsendScore(NETWORK *ir);
 
 int SaveIRID(int IRID, CSTR ID);

LR2/Scene02_Songselect.cpp
@@ -64,12 +64,7 @@ int SetTarget(game *g) {
 	return 1;
 }
 
-
-
-//401fe0
-void ThreadProc_LoadBanner(void *param) { // TODO: take game&
-	game* g = (game*)param;
-
+static void ThreadProc_LoadBanner(game* g) {
 	CSTR path;
 	CSTR dir(g->sSelect.bmsList[g->sSelect.cur_song].filepath.getDirectory());
 
@@ -85,7 +80,6 @@ void ThreadProc_LoadBanner(void *param) { // TODO: take game&
 			}
 		}
 	}
-	return;
 }
 
 //4021a0
@@ -1540,10 +1534,7 @@ void CheckNewSong(glb_dbgame *glb) {
 	(glb->pGame->sSelect).unk4fc4[2] = '\0';
 }
 
-//4181b0
-void ThreadProc_RankingAutoUpdate(void *param) { // TODO: take game&
-	game *g = (game*)param;
-
+static void ThreadProc_RankingAutoUpdate(game* g) {
 	CSTR hash = g->sSelect.bmsList[g->sSelect.cur_song].hash;
 	g->net.rankingData.Init();
 	g->net.rankingData.rivalID = 0;
@@ -1552,7 +1543,6 @@ void ThreadProc_RankingAutoUpdate(void *param) { // TODO: take game&
 	if (g->sSelect.stack_query[g->sSelect.cur].findStrPos("__RIVAL__") >= 0) {
 		g->net.rankingData.rivalID = g->sSelect.stack_rivalID[g->sSelect.cur];
 	}
-	g->sSelect.isRankingAutoUpdateThread = 1;
 	CSTR path;
 	if (hash.length() < 50) cstrSprintf(&path, "LR2files/Ir/%s.xml", hash.body);
 	else cstrSprintf(&path, "LR2files/Ir/%s.xml", hash.makeCRCstr().body);
@@ -1580,7 +1570,6 @@ void ThreadProc_RankingAutoUpdate(void *param) { // TODO: take game&
 		}
 
 		if (GetNowUnixtime() - GetFileUnixtime(path) < 86400 || g->config.network.autoupdate == 0) { //86400 is 24hours
-			g->sSelect.isRankingAutoUpdateThread = 0;
 			return;
 		}
 	}
@@ -1589,13 +1578,11 @@ void ThreadProc_RankingAutoUpdate(void *param) { // TODO: take game&
 
 	if (!g->config.network.autoupdate) {
 		if (!isIR2) g->net.IRstatus = 0;
-		g->sSelect.isRankingAutoUpdateThread = 0;
 		return;
 	}
 
 	if (g->net.rankUpdateDelayLevel >= 3) {
 		if (!isIR2) g->net.IRstatus = 5;
-		g->sSelect.isRankingAutoUpdateThread = 0;
 		return;
 	}
 
@@ -1604,7 +1591,6 @@ void ThreadProc_RankingAutoUpdate(void *param) { // TODO: take game&
 	while (GetTimeLapse(177, &g->timer1) <g->net.waitTime) {
 		std::this_thread::sleep_for(std::chrono::milliseconds(4));
 		if (g->net.waitForHandle || (isIR2 && (g->KeyInput.p1_buttonInput[4] == 2 || g->KeyInput.p2_buttonInput[4] == 2))) {
-			g->sSelect.isRankingAutoUpdateThread = 0;
 			ResetTimeLapse(177, &g->timer1);
 			g->net.IRstatus = isIR2 ? 2 : 0;
 			return;
@@ -1616,8 +1602,6 @@ void ThreadProc_RankingAutoUpdate(void *param) { // TODO: take game&
 	ErrorLogAdd("接続を開始します\n");
 
 	if (hash.isSame(g->sSelect.bmsList[g->sSelect.cur_song].hash) == 0) {
-		ErrorLogAdd("IRスレッド強制終了\n");
-		g->net.IRstatus = -3;
 		ErrorLogAdd("IRスレッド強制終了\n");
 		g->net.IRstatus = -3;
 	}
@@ -1654,15 +1638,11 @@ void ThreadProc_RankingAutoUpdate(void *param) { // TODO: take game&
 	}
 
 	SetObjectStrings_SongSelect(g);
-	g->sSelect.isRankingAutoUpdateThread = 0;
 }
 
-//419440
-int CountCallLoadPreview = 0;
-void LoadPreview(game *g) {
+static void ThreadProc_LoadPreview(game *g) {
 	BMSMETA meta;
-	
-	CountCallLoadPreview++;
+
 	if (!IsFileExist(g->gameplay.previewBMSfilepath)) {
 		g->gameplay.isPreviewLoad = 0;
 		g->gameplay.previewStatus = 0;
@@ -1702,8 +1682,6 @@ void LoadPreview(game *g) {
 		g->gameplay.isPreviewLoad = 0;
 	}
 }
-
-
 
 
 //41cb80
@@ -2681,7 +2659,7 @@ int ProcI_Select(game *g, sqlite3 *sql) {
 			g->gameplay.previewStatus = 1;
 			g->gameplay.previewBMShash = g->sSelect.bmsList[g->sSelect.cur_song].hash;
 			g->gameplay.previewBMSfilepath = g->sSelect.bmsList[g->sSelect.cur_song].filepath;
-			g->gameplay.hThreadPreview = std::jthread(LoadPreview, g);
+			g->gameplay.hThreadPreview = std::jthread(ThreadProc_LoadPreview, g);
 		}
 		else if (g->gameplay.previewStatus) {
 			if (g->gameplay.previewBMShash.isDiff(g->sSelect.bmsList[g->sSelect.cur_song].hash)) {

LR2/Scene02_Songselect.h
@@ -14,11 +14,6 @@ int SetBmsFilter(game * g, sqlite3 * sql);
 int GetSongCursor(game * g);
 
 int LoadFontForSongs(game * gs, char flag);
-void ThreadProc_LoadBanner(void * param);
-
-void ThreadProc_RankingAutoUpdate(void * param);
-
-void LoadPreview(game *g);
 
 //playoption
 int SetPlayOption(game * g, sqlite3 * sql);

LR2/structure.h
@@ -1031,7 +1031,6 @@ struct SONGSELECT {
 	double levelBarGraph[5];
 	double levelIndicatorAnimation[5];
 	struct CONFIG_SELECT filter;
-	char isRankingAutoUpdateThread; /* IR */
 	int rivalID;
 	struct COURSESELECT course;
 	int isExLevel;