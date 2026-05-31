chown2/OpenLR2

commit 6026df3c7cb3dd4d656b60740dbd84d04653b9be

refactor: add debug logs

LR2/LR2_bmsload.cpp
@@ -1803,6 +1803,7 @@ unsigned char channelConvert[] = { 0x00, 0x3c, 0x3c, 0x3c, 0x3c, 0x3c, 0x3c, 0x3
 									0x00, 0x3c, 0x3c, 0x3c, 0x3c, 0x3c, 0x31, 0x32, 0x33, 0x34,
 									0x00, 0x3c, 0x35, 0x36, 0x37, 0x38, 0x39, 0x00, 0x3a, 0x3b };
 int ParseBmsFile(gameplay *gp, CSTR filename, AUDIO *aud, ConfigStruct* cfg, BMSMETA *meta, int bgaFlag, int scratchSide) {
+	ErrorLogFmtAdd("ParseBmsFile(%s)\n", filename);
 
 	std::unordered_map<double, int> notesPerBpm{};
 	double avgBPM_bpmsum{};

LR2/En_fileutil.cpp
@@ -834,6 +834,8 @@ int FindAltSound(CSTR filename, CSTR dir, CSTR *oBuf) {
 		}
 	}
 
+	ErrorLogFmtAdd("FindAltSound: '%s' not found in '%s'\n", filename.c_str(), dir.c_str());
+
 	oBuf->fillzero();
 	return 0;
 }

commit 27e7a65a194bddc0a4e2bd95a1f9ec1866f519b8

fix: don't pass CSTR in variadics
Fixes: 6026df3c7cb3 ("refactor: add debug logs")

LR2/LR2_bmsload.cpp
@@ -1809,7 +1809,7 @@ unsigned char channelConvert[] = { 0x00, 0x3c, 0x3c, 0x3c, 0x3c, 0x3c, 0x3c, 0x3
 									0x00, 0x3c, 0x3c, 0x3c, 0x3c, 0x3c, 0x31, 0x32, 0x33, 0x34,
 									0x00, 0x3c, 0x35, 0x36, 0x37, 0x38, 0x39, 0x00, 0x3a, 0x3b };
 int ParseBmsFile(gameplay *gp, CSTR filename, AUDIO *aud, ConfigStruct* cfg, BMSMETA *meta, int bgaFlag, int scratchSide) {
-	ErrorLogFmtAdd("ParseBmsFile(%s)\n", filename);
+	ErrorLogFmtAdd("ParseBmsFile(%s)\n", filename.c_str());
 
 	std::unordered_map<double, int> notesPerBpm{};
 	double avgBPM_bpmsum{};