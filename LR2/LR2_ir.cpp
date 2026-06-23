chown2/OpenLR2

commit e7f1463d02b37a7bae6b9f75b1f49c7a644e53e7

fix: fix remaining CSTR being passed as variadic

LR2/LR2_ir.cpp
@@ -576,7 +576,7 @@ int NETWORK::HTTPrequest() {
 		ioctlsocket(s, 0x8004667e, &argp);
 
 		request.fillzero();
-		cstrSprintf(&request, "POST %s HTTP/1.0\r\nContent-Length:%d\n\n%s", this->target_URL, this->param.length(), this->param);
+		cstrSprintf(&request, "POST %s HTTP/1.0\r\nContent-Length:%d\n\n%s", this->target_URL.body, this->param.length(), this->param.body);
 
 		if (send(s, request, request.length() + 1, 0) < 0) {
 			cstrSprintf(&this->request_debug, "データの送信に失敗しました : %d\n", WSAGetLastError());

LR2/En_fileutil.cpp
@@ -82,7 +82,7 @@ time_t GetFileUnixtime(CSTR str) {
 	LPWIN32_FIND_DATAA lpFindFileData = (LPWIN32_FIND_DATAA)&FindFileData;
 	HANDLE hFindFile = FindFirstFileA(str, lpFindFileData);
 	if (hFindFile == (HANDLE)-1) {
-		ErrorLogFmtAdd("ファイルのLR2TIME取得エラー:%sが見つからない\n", str);
+		ErrorLogFmtAdd("ファイルのLR2TIME取得エラー:%sが見つからない\n", str.body);
 		return -1;
 	}
 

SkinEditor/winWorkspace.cpp
@@ -283,7 +283,7 @@ int WORKSPACE::drawCustomize() {
     for (int i = 0; i < meta.custom_count; i++) {
         SkinCustom& cu = meta.customs[i];
 
-        ImGui::Text("%s", cu.title);
+        ImGui::TextUnformatted(cu.title);
         ImGui::SameLine();
         
         char item[64];
@@ -401,9 +401,9 @@ int WORKSPACE::drawTextEdit() {
                 {
                     ImGui::TableSetColumnIndex(column);
                     if(read.csv.str[column].atPos(0) == nullptr)
-                        ImGui::TextDisabled("%s", read.csv.str[column]);
+                        ImGui::TextDisabled("%s", read.csv.str[column].body);
                     else
-                        ImGui::Text("%s", read.csv.str[column]);
+                        ImGui::TextUnformatted(read.csv.str[column]);
                 }
                 ImGui::EndTable();
             }

commit b295df7f27bef527b52ea1def2a44256b47caf5f

fix(ir): pass CSTR().body in variadics (RE issue)
As it turns out, all of these are also broken on MSVC and hence are RE
issues, instead of being an allowed MSVC quirk.
f16d1c64ce22, e7f1463d02b3, and this.

Fixes: e7f1463d02b3 ("fix: fix remaining CSTR being passed as variadic")

LR2/LR2_ir.cpp
@@ -835,7 +835,7 @@ int NETWORK::LR2IR_GetTargetInfo(int mode, CSTR songmd5, CSTR *oData, CSTR *oNam
 				ErrorLogAdd("ゴーストのダウンロードに失敗しました\n");
 				return 0;
 			}
-			ErrorLogFmtAdd("ゴーストをダウンロードしました\nプレイヤー名%s\n", oName);
+			ErrorLogFmtAdd("ゴーストをダウンロードしました\nプレイヤー名%s\n", oName->body);
 		}
 		return 1;
 	}