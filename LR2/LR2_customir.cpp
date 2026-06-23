chown2/OpenLR2

commit 4ec48ab05db2e933c3ddcbbdd98831171428177a

feat: custom IR API
Co-authored-by: chown2 <kuso@cock.email>

LR2/LR2_customir.cpp
@@ -1,0 +1,482 @@
+#include "LR2_customir.h"
+#include "LR2_customir_api.h"
+#include "structure.h"
+#include "LR2_songmanage.h"
+
+#include <filesystem>
+#include <format>
+#include <future>
+#include <queue>
+#include <ranges>
+#include <thread>
+#include <type_traits>
+
+#include <DxLib/DxLib.h>
+#include <libloaderapi.h>
+
+#if _WIN64
+#if _DEBUG
+constexpr auto&& ARCH = "_D.x64";
+#else
+constexpr auto&& ARCH = ".x64";
+#endif
+#elif _WIN32
+#if _DEBUG
+constexpr auto&& ARCH = "_D.x86";
+#else
+constexpr auto&& ARCH = ".x86";
+#endif
+#else
+#error "Unsupported Arch Linux"
+#endif
+
+// TODO
+#define OverlayNotification ErrorLogFmtAdd
+
+CustomIR::CustomIR(const std::filesystem::path& _directory) {
+	for (auto& file : std::filesystem::directory_iterator(_directory)) {
+		if (!file.is_regular_file()) continue;
+		if (file.path().extension().string() != ".dll") continue;
+		if (auto s = file.path().stem().string(); !s.ends_with(ARCH)) {
+			ErrorLogFmtAdd("'%s' skipping IR module with invalid file name stem (expected %s)\n", s.c_str(), ARCH);
+			continue;
+		}
+		mDllHandle.reset(LoadLibraryW(file.path().wstring().c_str()));
+		if (mDllHandle == nullptr) continue;
+		auto GetMethodTable = reinterpret_cast<void (__cdecl*)(MethodTable&)>(GetProcAddress(mDllHandle.get(), "GetMethodTable"));
+		if (GetMethodTable == nullptr) {
+			ErrorLogFmtAdd("'%s' not loaded, missing 'GetMethodTable' export.\n", file.path().filename().string().c_str());
+			continue;
+		};
+		GetMethodTable(mMethods);
+		if (mMethods.GetName == nullptr) {
+			ErrorLogFmtAdd("'%s' not loaded, missing essential 'GetName' implementation.\n", file.path().filename().string().c_str());
+			continue;
+		};
+		ErrorLogFmtAdd("'%s' loaded: %s\n", file.path().filename().string().c_str(), mMethods.GetName());
+		break;
+	}
+}
+
+void CustomIR::ModuleDeleter::operator()(std::remove_pointer_t<HMODULE>* handle) {
+	FreeLibrary(handle);
+}
+
+bool CustomIR::Initialize() {
+	mName = mMethods.GetName();
+	if (mName.empty()) return false;
+	return true;
+}
+
+bool CustomIR::Login() {
+	if (mMethods.LoginV1 == nullptr) return true;
+	return mMethods.LoginV1();
+}
+
+SendScoreStatus CustomIR::SendScore(const IRScoreV1& score) {
+	if (mMethods.SendScoreV1 == nullptr) return SendScoreStatus::Fail;
+	return mMethods.SendScoreV1(score);
+}
+
+CUSTOMIR_MANAGER::~CUSTOMIR_MANAGER() {
+	// Make sure all scores are sent before exiting out of the process. (Can hang for up to ~15 minutes...)
+	for (auto& thread : mSendThreads) {
+		thread.get();
+	}
+}
+
+void CUSTOMIR_MANAGER::Initialize(const std::filesystem::path& directory) {
+	for (auto& dir : std::filesystem::directory_iterator(directory)) {
+		if (!dir.is_directory()) {
+			ErrorLogFmtAdd("'%s' skipped for loading custom IR module, not a directory\n", dir.path().string().c_str());
+			continue;
+		}
+		auto& ir = *mModules.emplace_back(std::make_shared<CustomIR>(dir));
+		if (!ir.Initialize()) {
+			mModules.pop_back();
+			ErrorLogFmtAdd("'%s' failed to initialize as a custom IR module\n", dir.path().string().c_str());
+			continue;
+		}
+		if (std::ranges::contains(std::ranges::subrange(mModules.begin(), mModules.end() - 1), ir.Name(), &CustomIR::Name)) {
+			ErrorLogFmtAdd("'%s' IR module with such name has already been loaded\n");
+			mModules.pop_back();
+			continue;
+		}
+	}
+}
+
+void CUSTOMIR_MANAGER::Login() {
+	for (auto& ir : mModules) {
+		if (ir->Login()) {
+			OverlayNotification("[%s] Logged in\n", ir->Name().c_str());
+		} else {
+			OverlayNotification("[%s] Failed to log in\n", ir->Name().c_str());
+		}
+	}
+}
+
+struct IRScoreInternal {
+	struct SONG {
+		std::string hash;
+		std::string title;
+		std::string subtitle;
+		std::string genre;
+		std::string artist;
+		std::string subartist;
+		int maxBPM{};
+		int minBPM{};
+		int longnote{};
+		int random{};
+		int judge{};
+		int courseStageCount{};
+		int courseType{};
+	} song{};
+	struct SETTINGS {
+		int gaugeOption{};
+		std::array<int, 2> random{};
+		int autokey{};
+		std::array<int, 2> assist{};
+		int dpflip{};
+		int hsfix{};
+		std::array<int, 2> randSC{};
+		std::array<int, 2> randFix{};
+		int m_softlanding{};
+		int m_addmine{};
+		int m_addlong{};
+		int m_earthquake{};
+		int m_tornado{};
+		int m_superloop{};
+		int m_gambol{};
+		int m_char{};
+		int m_heartbeat{};
+		int m_loudness{};
+		int m_addnote{};
+		int m_nabeatsu{};
+		int m_accel{};
+		int m_sincurve{};
+		int m_wave{};
+		int m_spiral{};
+		int m_sidejump{};
+		int is_extra{};
+		int m_extra{};
+		char m_lunaris{};
+		bool m_gas{};
+		int gomiscore{};
+		int disablecurspeedchange{};
+	} settings{};
+	struct STATE {
+		int player{};
+		int keymode{};
+		int randomseed{};
+		double freqSpeedMultiplier{};
+		double song_runtime{};
+		char isNosave{};
+		char isForceEasy{};
+		char isCourse{};
+		int courseStageNow{};
+		int notes_total{};
+	} state{};
+	struct JUDGEMENTS {
+		unsigned int epg{};
+		unsigned int lpg{};
+		unsigned int egr{};
+		unsigned int lgr{};
+		unsigned int egd{};
+		unsigned int lgd{};
+		unsigned int ebd{};
+		unsigned int lbd{};
+		unsigned int epr{};
+		unsigned int lpr{};
+		unsigned int cb{};
+		unsigned int fast{};
+		unsigned int slow{};
+		unsigned int notes_played{};
+		unsigned int notes_total{};
+	};
+	JUDGEMENTS judgements_total{};
+	std::array<JUDGEMENTS, 20> judgements_column{};
+	unsigned int max_combo{};
+	std::array<double, 6> HP{};
+	int gaugeType{};
+	int moneyscore{};
+	int exscore{};
+	double rate{};
+	int clearType{};
+	int inputType{};
+	struct GRAPHDATA {
+		std::array<std::array<int, 1000>, 6> hp{};
+		std::array<int, 1000> combo{};
+		std::array<int, 1000> exscore{};
+		std::array<int, 1000> rate{};
+	} graphs{};
+
+	IRScoreInternal(game& game, sqlite3* sql, int player);
+	void MakeScoreV1(IRScoreV1& scoreOut);
+};
+
+void IRScoreInternal::MakeScoreV1(IRScoreV1& scoreOut) {
+	scoreOut.song.hash = song.hash;
+	scoreOut.song.title = song.title;
+	scoreOut.song.subtitle = song.subtitle;
+	scoreOut.song.genre = song.genre;
+	scoreOut.song.artist = song.artist;
+	scoreOut.song.subartist = song.subartist;
+	scoreOut.song.maxBPM = song.maxBPM;
+	scoreOut.song.minBPM = song.minBPM;
+	scoreOut.song.longnote = song.longnote;
+	scoreOut.song.random = song.random;
+	scoreOut.song.judge = song.judge;
+	scoreOut.song.courseStageCount = song.courseStageCount;
+	scoreOut.song.courseType = song.courseType;
+
+	scoreOut.settings.gaugeOption = settings.gaugeOption;
+	scoreOut.settings.random = settings.random;
+	scoreOut.settings.autokey = settings.autokey;
+	scoreOut.settings.assist = settings.assist;
+	scoreOut.settings.dpflip = settings.dpflip;
+	scoreOut.settings.hsfix = settings.hsfix;
+	scoreOut.settings.randSC = settings.randSC;
+	scoreOut.settings.randFix = settings.randFix;
+	scoreOut.settings.m_softlanding = settings.m_softlanding;
+	scoreOut.settings.m_addmine = settings.m_addmine;
+	scoreOut.settings.m_addlong = settings.m_addlong;
+	scoreOut.settings.m_earthquake = settings.m_earthquake;
+	scoreOut.settings.m_tornado = settings.m_tornado;
+	scoreOut.settings.m_superloop = settings.m_superloop;
+	scoreOut.settings.m_gambol = settings.m_gambol;
+	scoreOut.settings.m_char = settings.m_char;
+	scoreOut.settings.m_heartbeat = settings.m_heartbeat;
+	scoreOut.settings.m_loudness = settings.m_loudness;
+	scoreOut.settings.m_addnote = settings.m_addnote;
+	scoreOut.settings.m_nabeatsu = settings.m_nabeatsu;
+	scoreOut.settings.m_accel = settings.m_accel;
+	scoreOut.settings.m_sincurve = settings.m_sincurve;
+	scoreOut.settings.m_wave = settings.m_wave;
+	scoreOut.settings.m_spiral = settings.m_spiral;
+	scoreOut.settings.m_sidejump = settings.m_sidejump;
+	scoreOut.settings.is_extra = settings.is_extra;
+	scoreOut.settings.m_extra = settings.m_extra;
+	scoreOut.settings.m_lunaris = settings.m_lunaris;
+	scoreOut.settings.m_gas = settings.m_gas;
+	scoreOut.settings.gomiscore = settings.gomiscore;
+	scoreOut.settings.disablecurspeedchange = settings.disablecurspeedchange;
+
+	scoreOut.state.player = state.player;
+	scoreOut.state.keymode = state.keymode;
+	scoreOut.state.randomseed = state.randomseed;
+	scoreOut.state.freqSpeedMultiplier = state.freqSpeedMultiplier;
+	scoreOut.state.song_runtime = state.song_runtime;
+	scoreOut.state.isNosave = state.isNosave;
+	scoreOut.state.isForceEasy = state.isForceEasy;
+	scoreOut.state.isCourse = state.isCourse;
+	scoreOut.state.courseStageNow = state.courseStageNow;
+	scoreOut.state.notes_total = state.notes_total;
+
+	auto judgements_assign = [](IRScoreV1::JUDGEMENTS& out, const IRScoreInternal::JUDGEMENTS& in) {
+		out.epg = in.epg;
+		out.lpg = in.lpg;
+		out.egr = in.egr;
+		out.lgr = in.lgr;
+		out.egd = in.egd;
+		out.lgd = in.lgd;
+		out.ebd = in.ebd;
+		out.lbd = in.lbd;
+		out.epr = in.epr;
+		out.lpr = in.lpr;
+		out.cb = in.cb;
+		out.fast = in.fast;
+		out.slow = in.slow;
+		out.notes_played = in.notes_played;
+		out.notes_total = in.notes_total;
+	};
+	judgements_assign(scoreOut.judgements_total, judgements_total);
+	for (std::size_t i = 0; i < scoreOut.judgements_column.size(); i++) {
+		judgements_assign(scoreOut.judgements_column[i], judgements_column[i]);
+	}
+	scoreOut.max_combo = max_combo;
+	scoreOut.HP = HP;
+	scoreOut.gaugeType = gaugeType;
+	scoreOut.moneyscore = moneyscore;
+	scoreOut.exscore = exscore;
+	scoreOut.rate = rate;
+	scoreOut.clearType = clearType;
+	scoreOut.inputType = inputType;
+
+	scoreOut.graphs.hp = graphs.hp;
+	scoreOut.graphs.combo = graphs.combo;
+	scoreOut.graphs.exscore = graphs.exscore;
+	scoreOut.graphs.rate = graphs.rate;
+}
+
+IRScoreInternal::IRScoreInternal(game& game, sqlite3* sql, int _player) {
+	const SONGDATA& curSong = game.sSelect.bmsList[game.sSelect.cur_song];
+	bool courseSong = (game.gameplay.courseType == 0 || game.gameplay.courseType == 2) && game.procSelecter != 13;
+	bool courseScore = game.procSelecter == 13;
+	if (courseSong) {
+		song.hash = curSong.courseHash[game.gameplay.courseStageNow].body;
+		SONGDATA songData;
+		GetSongData(song.hash.c_str(), &songData, sql, &game.sSelect);
+		song.title = songData.title.body;
+		song.subtitle = songData.subtitle.body;
+		song.genre = songData.genre.body;
+		song.artist = songData.artist.body;
+		song.subartist = songData.subartist.body;
+		song.maxBPM = songData.maxBPM;
+		song.minBPM = songData.minBPM;
+		song.longnote = songData.longnote;
+		song.random = songData.random;
+		song.judge = songData.judge;
+	}
+	else {
+		song.hash = curSong.hash.body;
+		song.title = curSong.title.body;
+		song.subtitle = curSong.subtitle.body;
+		song.genre = curSong.genre.body;
+		song.artist = curSong.artist.body;
+		song.subartist = curSong.subartist.body;
+		song.maxBPM = curSong.maxBPM;
+		song.minBPM = curSong.minBPM;
+		song.longnote = curSong.longnote;
+		song.random = curSong.random;
+		song.judge = curSong.judge;
+		song.courseStageCount = curSong.courseStageCount;
+		song.courseType = curSong.courseType;
+	}
+	CONFIG_PLAY& cfg = game.config.play;
+	settings.gaugeOption = cfg.gaugeOption[_player];
+	settings.random[0] = cfg.random[0];
+	settings.random[1] = cfg.random[1];
+	settings.autokey = cfg.autokey;
+	settings.assist[0] = cfg.p1_assist;
+	settings.assist[1] = cfg.p2_assist;
+	settings.dpflip = cfg.dpflip;
+	settings.hsfix = cfg.hsfix;
+	settings.randSC[0] = cfg.randSC[0];
+	settings.randSC[1] = cfg.randSC[1];
+	settings.randFix[0] = cfg.randFix[0];
+	settings.randFix[1] = cfg.randFix[1];
+	settings.m_softlanding = cfg.m_softlanding;
+	settings.m_addmine = cfg.m_addmine;
+	settings.m_addlong = cfg.m_addlong;
+	settings.m_earthquake = cfg.m_earthquake;
+	settings.m_tornado = cfg.m_tornado;
+	settings.m_superloop = cfg.m_superloop;
+	settings.m_gambol = cfg.m_gambol;
+	settings.m_char = cfg.m_char;
+	settings.m_heartbeat = cfg.m_heartbeat;
+	settings.m_loudness = cfg.m_loudness;
+	settings.m_addnote = cfg.m_addnote;
+	settings.m_nabeatsu = cfg.m_nabeatsu;
+	settings.m_accel = cfg.m_accel;
+	settings.m_sincurve = cfg.m_sincurve;
+	settings.m_wave = cfg.m_wave;
+	settings.m_spiral = cfg.m_spiral;
+	settings.m_sidejump = cfg.m_sidejump;
+	settings.is_extra = cfg.is_extra;
+	settings.m_extra = cfg.m_extra;
+	settings.m_lunaris = cfg.m_lunaris;
+	settings.m_gas = cfg.m_gas;
+	settings.gomiscore = cfg.gomiscore;
+	settings.disablecurspeedchange = cfg.disablecurspeedchange;
+
+	gameplay& gameplay = game.gameplay;
+	state.player = _player;
+	state.keymode = gameplay.keymode;
+	state.randomseed = gameplay.randomseed;
+	state.freqSpeedMultiplier = gameplay.freqSpeedMultiplier;
+	if (!courseScore) state.song_runtime = gameplay.song_runtime;
+	state.isNosave = gameplay.isNosave;
+	state.isForceEasy = gameplay.isForceEasy;
+	state.isCourse = gameplay.isCourse;
+	state.courseStageNow = gameplay.courseStageNow;
+	state.notes_total = gameplay.player[_player].totalnotes;
+
+	PLAYERSTATUS& player = gameplay.player[_player];
+	auto judgements_assign = [](IRScoreInternal::JUDGEMENTS& out, const EXTENDEDPLAYERSTATS& in) {
+		out.epg = in.epg;
+		out.lpg = in.lpg;
+		out.egr = in.egr;
+		out.lgr = in.lgr;
+		out.egd = in.egd;
+		out.lgd = in.lgd;
+		out.ebd = in.ebd;
+		out.lbd = in.lbd;
+		out.epr = in.epr;
+		out.lpr = in.lpr;
+		out.cb = in.cb;
+		out.fast = in.fast;
+		out.slow = in.slow;
+		out.notes_played = in.noteCount;
+		};
+	if (!courseScore) {
+		judgements_assign(judgements_total, player.extendedStats);
+		judgements_total.notes_total = state.notes_total;
+		for (std::size_t i = 0; i < judgements_column.size(); i++) {
+			judgements_assign(judgements_column[i], player.extendedColumnStats[i]);
+			judgements_column[i].notes_total = gameplay.bmsobj_note[i].count;
+		}
+	}
+	else {
+		judgements_assign(judgements_total, player.extendedStatsCourse);
+		judgements_total.notes_total = state.notes_total;
+		for (std::size_t i = 0; i < judgements_column.size(); i++) {
+			judgements_assign(judgements_column[i], player.extendedColumnStatsCourse[i]);
+			judgements_column[i].notes_total = gameplay.bmsobj_note[i].count;
+		}
+	}
+	max_combo = player.max_combo;
+	HP = player.HP;
+	gaugeType = player.gaugeType;
+	if (!courseScore) moneyscore = player.score;
+	exscore = player.exscore;
+	rate = player.rate;
+	clearType = player.clearType;
+	inputType = DetermineResultPlayDevice(&game.KeyInput);
+
+	if (!courseScore) {
+		::GRAPHDATA& statgraph = gameplay.statgraph[_player];
+		graphs.hp = statgraph.hp;
+		memcpy(graphs.combo.data(), statgraph.combo, graphs.combo.size());
+		memcpy(graphs.exscore.data(), statgraph.exscore, graphs.exscore.size());
+		memcpy(graphs.rate.data(), gameplay.rategraph[_player].val, graphs.rate.size());
+	}
+}
+
+void CUSTOMIR_MANAGER::SendScore(game& game, sqlite3* sql, int player) {
+	// Deferred deletion because we need to keep the std::future for whatever reason
+	std::vector<int> finishedThreads;
+	for (const auto& [i, it] : std::views::enumerate(mSendThreads)) {
+		if (it.wait_for(std::chrono::milliseconds(0)) == std::future_status::ready) finishedThreads.push_back(i);
+	}
+	std::ranges::reverse(finishedThreads);
+	for (auto i : finishedThreads) {
+		mSendThreads.erase(mSendThreads.begin() + i);
+	}
+	mSendThreads.push_back(std::async(std::launch::async, [](IRScoreInternal internal, std::vector<std::shared_ptr<CustomIR>> toSend) {
+		IRScoreV1 scoreV1;
+		internal.MakeScoreV1(scoreV1);
+		constexpr const int tryMax = 6;
+		int tryCount = 1;
+		while (!toSend.empty() && tryCount <= tryMax) {
+			std::vector<std::future<SendScoreStatus>> sendThreads;
+			sendThreads.reserve(toSend.size());
+			for (const auto& module : toSend) {
+				sendThreads.push_back(std::async(std::launch::async, &CustomIR::SendScore, module, scoreV1));
+			}
+
+			for (const auto& [i, t] : std::views::enumerate(sendThreads) | std::views::reverse) {
+				switch (t.get()) {
+				case SendScoreStatus::Fail:
+					OverlayNotification("'%s' failed to submit score\n", toSend[i]->Name().c_str());
+					[[fallthrough]];
+				case SendScoreStatus::Ok: toSend.erase(toSend.begin() + i); break;
+				case SendScoreStatus::Retry: break;
+				}
+			}
+
+			const auto sleepFor = static_cast<int>(std::pow(4, tryCount));
+			std::this_thread::sleep_for(std::chrono::seconds(sleepFor));
+			tryCount++;
+		}
+	}, IRScoreInternal{ game, sql, player }, mModules));
+}

CMakeLists.txt
@@ -64,6 +64,7 @@ add_library(
     LR2/LR2_audio.cpp
     LR2/LR2_bmsload.cpp
     LR2/LR2_configsave.cpp
+    LR2/LR2_customir.cpp
     LR2/LR2_gameloop.cpp
     LR2/LR2_ghost.cpp
     LR2/LR2_ir.cpp

ExampleIR/ExampleIR.vcxproj
@@ -1,0 +1,190 @@
+<?xml version="1.0" encoding="utf-8"?>
+<Project DefaultTargets="Build" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+  <ItemGroup Label="ProjectConfigurations">
+    <ProjectConfiguration Include="Debug|Win32">
+      <Configuration>Debug</Configuration>
+      <Platform>Win32</Platform>
+    </ProjectConfiguration>
+    <ProjectConfiguration Include="Release|Win32">
+      <Configuration>Release</Configuration>
+      <Platform>Win32</Platform>
+    </ProjectConfiguration>
+    <ProjectConfiguration Include="Debug|x64">
+      <Configuration>Debug</Configuration>
+      <Platform>x64</Platform>
+    </ProjectConfiguration>
+    <ProjectConfiguration Include="Release|x64">
+      <Configuration>Release</Configuration>
+      <Platform>x64</Platform>
+    </ProjectConfiguration>
+  </ItemGroup>
+  <PropertyGroup Label="Globals">
+    <VCProjectVersion>17.0</VCProjectVersion>
+    <Keyword>Win32Proj</Keyword>
+    <ProjectGuid>{bf127ba9-475c-4bec-91f6-058d5d7a2f65}</ProjectGuid>
+    <RootNamespace>ExampleIR</RootNamespace>
+    <WindowsTargetPlatformVersion>10.0</WindowsTargetPlatformVersion>
+  </PropertyGroup>
+  <Import Project="$(VCTargetsPath)\Microsoft.Cpp.Default.props" />
+  <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Debug|Win32'" Label="Configuration">
+    <ConfigurationType>DynamicLibrary</ConfigurationType>
+    <UseDebugLibraries>true</UseDebugLibraries>
+    <PlatformToolset>v143</PlatformToolset>
+    <CharacterSet>Unicode</CharacterSet>
+  </PropertyGroup>
+  <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Release|Win32'" Label="Configuration">
+    <ConfigurationType>DynamicLibrary</ConfigurationType>
+    <UseDebugLibraries>false</UseDebugLibraries>
+    <PlatformToolset>v143</PlatformToolset>
+    <WholeProgramOptimization>true</WholeProgramOptimization>
+    <CharacterSet>Unicode</CharacterSet>
+  </PropertyGroup>
+  <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Debug|x64'" Label="Configuration">
+    <ConfigurationType>DynamicLibrary</ConfigurationType>
+    <UseDebugLibraries>true</UseDebugLibraries>
+    <PlatformToolset>v143</PlatformToolset>
+    <CharacterSet>Unicode</CharacterSet>
+  </PropertyGroup>
+  <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Release|x64'" Label="Configuration">
+    <ConfigurationType>DynamicLibrary</ConfigurationType>
+    <UseDebugLibraries>false</UseDebugLibraries>
+    <PlatformToolset>v143</PlatformToolset>
+    <WholeProgramOptimization>true</WholeProgramOptimization>
+    <CharacterSet>Unicode</CharacterSet>
+  </PropertyGroup>
+  <Import Project="$(VCTargetsPath)\Microsoft.Cpp.props" />
+  <ImportGroup Label="ExtensionSettings">
+  </ImportGroup>
+  <ImportGroup Label="Shared">
+  </ImportGroup>
+  <ImportGroup Label="PropertySheets" Condition="'$(Configuration)|$(Platform)'=='Debug|Win32'">
+    <Import Project="$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props" Condition="exists('$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props')" Label="LocalAppDataPlatform" />
+  </ImportGroup>
+  <ImportGroup Label="PropertySheets" Condition="'$(Configuration)|$(Platform)'=='Release|Win32'">
+    <Import Project="$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props" Condition="exists('$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props')" Label="LocalAppDataPlatform" />
+  </ImportGroup>
+  <ImportGroup Label="PropertySheets" Condition="'$(Configuration)|$(Platform)'=='Debug|x64'">
+    <Import Project="$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props" Condition="exists('$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props')" Label="LocalAppDataPlatform" />
+  </ImportGroup>
+  <ImportGroup Label="PropertySheets" Condition="'$(Configuration)|$(Platform)'=='Release|x64'">
+    <Import Project="$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props" Condition="exists('$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props')" Label="LocalAppDataPlatform" />
+  </ImportGroup>
+  <PropertyGroup Label="UserMacros" />
+  <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Debug|Win32'">
+    <OutDir>$(ProjectDir)bin\</OutDir>
+    <IntDir>$(ProjectDir)build\$(Platform)\$(Configuration)\</IntDir>
+    <TargetName>ExampleIR_D.x86</TargetName>
+  </PropertyGroup>
+  <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Release|Win32'">
+    <OutDir>$(ProjectDir)bin\</OutDir>
+    <IntDir>$(ProjectDir)build\$(Platform)\$(Configuration)\</IntDir>
+    <TargetName>ExampleIR.x86</TargetName>
+  </PropertyGroup>
+  <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Debug|x64'">
+    <OutDir>$(ProjectDir)bin\</OutDir>
+    <IntDir>$(ProjectDir)build\$(Platform)\$(Configuration)\</IntDir>
+    <TargetName>ExampleIR_D.x64</TargetName>
+  </PropertyGroup>
+  <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Release|x64'">
+    <OutDir>$(ProjectDir)bin\</OutDir>
+    <IntDir>$(ProjectDir)build\$(Platform)\$(Configuration)\</IntDir>
+    <TargetName>ExampleIR.x64</TargetName>
+  </PropertyGroup>
+  <PropertyGroup Label="Vcpkg">
+    <VcpkgEnabled>false</VcpkgEnabled>
+  </PropertyGroup>
+  <ItemDefinitionGroup Condition="'$(Configuration)|$(Platform)'=='Debug|Win32'">
+    <ClCompile>
+      <WarningLevel>Level3</WarningLevel>
+      <SDLCheck>true</SDLCheck>
+      <PreprocessorDefinitions>NOMINMAX;WIN32_LEAN_AND_MEAN;WIN32;_DEBUG;EXAMPLEIR_EXPORTS;_WINDOWS;_USRDLL;%(PreprocessorDefinitions)</PreprocessorDefinitions>
+      <ConformanceMode>true</ConformanceMode>
+      <PrecompiledHeader>NotUsing</PrecompiledHeader>
+      <PrecompiledHeaderFile>pch.h</PrecompiledHeaderFile>
+      <LanguageStandard>stdcpp23</LanguageStandard>
+      <LanguageStandard_C>stdc17</LanguageStandard_C>
+      <AdditionalIncludeDirectories>$(SolutionDir)LR2\;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
+      <MultiProcessorCompilation>true</MultiProcessorCompilation>
+      <BufferSecurityCheck>
+      </BufferSecurityCheck>
+    </ClCompile>
+    <Link>
+      <SubSystem>Windows</SubSystem>
+      <GenerateDebugInformation>true</GenerateDebugInformation>
+      <EnableUAC>false</EnableUAC>
+    </Link>
+  </ItemDefinitionGroup>
+  <ItemDefinitionGroup Condition="'$(Configuration)|$(Platform)'=='Release|Win32'">
+    <ClCompile>
+      <WarningLevel>Level3</WarningLevel>
+      <FunctionLevelLinking>true</FunctionLevelLinking>
+      <IntrinsicFunctions>true</IntrinsicFunctions>
+      <SDLCheck>true</SDLCheck>
+      <PreprocessorDefinitions>NOMINMAX;WIN32_LEAN_AND_MEAN;WIN32;NDEBUG;EXAMPLEIR_EXPORTS;_WINDOWS;_USRDLL;%(PreprocessorDefinitions)</PreprocessorDefinitions>
+      <ConformanceMode>true</ConformanceMode>
+      <PrecompiledHeader>NotUsing</PrecompiledHeader>
+      <PrecompiledHeaderFile>pch.h</PrecompiledHeaderFile>
+      <LanguageStandard>stdcpp23</LanguageStandard>
+      <LanguageStandard_C>stdc17</LanguageStandard_C>
+      <AdditionalIncludeDirectories>$(SolutionDir)LR2\;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
+      <MultiProcessorCompilation>true</MultiProcessorCompilation>
+      <BufferSecurityCheck>
+      </BufferSecurityCheck>
+    </ClCompile>
+    <Link>
+      <SubSystem>Windows</SubSystem>
+      <GenerateDebugInformation>true</GenerateDebugInformation>
+      <EnableUAC>false</EnableUAC>
+    </Link>
+  </ItemDefinitionGroup>
+  <ItemDefinitionGroup Condition="'$(Configuration)|$(Platform)'=='Debug|x64'">
+    <ClCompile>
+      <WarningLevel>Level3</WarningLevel>
+      <SDLCheck>true</SDLCheck>
+      <PreprocessorDefinitions>NOMINMAX;WIN32_LEAN_AND_MEAN;_DEBUG;EXAMPLEIR_EXPORTS;_WINDOWS;_USRDLL;%(PreprocessorDefinitions)</PreprocessorDefinitions>
+      <ConformanceMode>true</ConformanceMode>
+      <PrecompiledHeader>NotUsing</PrecompiledHeader>
+      <PrecompiledHeaderFile>pch.h</PrecompiledHeaderFile>
+      <LanguageStandard>stdcpp23</LanguageStandard>
+      <LanguageStandard_C>stdc17</LanguageStandard_C>
+      <AdditionalIncludeDirectories>$(SolutionDir)LR2\;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
+      <MultiProcessorCompilation>true</MultiProcessorCompilation>
+      <BufferSecurityCheck>
+      </BufferSecurityCheck>
+    </ClCompile>
+    <Link>
+      <SubSystem>Windows</SubSystem>
+      <GenerateDebugInformation>true</GenerateDebugInformation>
+      <EnableUAC>false</EnableUAC>
+    </Link>
+  </ItemDefinitionGroup>
+  <ItemDefinitionGroup Condition="'$(Configuration)|$(Platform)'=='Release|x64'">
+    <ClCompile>
+      <WarningLevel>Level3</WarningLevel>
+      <FunctionLevelLinking>true</FunctionLevelLinking>
+      <IntrinsicFunctions>true</IntrinsicFunctions>
+      <SDLCheck>true</SDLCheck>
+      <PreprocessorDefinitions>NOMINMAX;WIN32_LEAN_AND_MEAN;NDEBUG;EXAMPLEIR_EXPORTS;_WINDOWS;_USRDLL;%(PreprocessorDefinitions)</PreprocessorDefinitions>
+      <ConformanceMode>true</ConformanceMode>
+      <PrecompiledHeader>NotUsing</PrecompiledHeader>
+      <PrecompiledHeaderFile>pch.h</PrecompiledHeaderFile>
+      <LanguageStandard>stdcpp23</LanguageStandard>
+      <LanguageStandard_C>stdc17</LanguageStandard_C>
+      <AdditionalIncludeDirectories>$(SolutionDir)LR2\;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
+      <MultiProcessorCompilation>true</MultiProcessorCompilation>
+      <BufferSecurityCheck>
+      </BufferSecurityCheck>
+    </ClCompile>
+    <Link>
+      <SubSystem>Windows</SubSystem>
+      <GenerateDebugInformation>true</GenerateDebugInformation>
+      <EnableUAC>false</EnableUAC>
+    </Link>
+  </ItemDefinitionGroup>
+  <ItemGroup>
+    <ClCompile Include="ExternalIR.cpp" />
+  </ItemGroup>
+  <Import Project="$(VCTargetsPath)\Microsoft.Cpp.targets" />
+  <ImportGroup Label="ExtensionTargets">
+  </ImportGroup>
+</Project>

ExampleIR/ExampleIR.vcxproj.filters
@@ -1,0 +1,22 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<Project ToolsVersion="4.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+  <ItemGroup>
+    <Filter Include="Source Files">
+      <UniqueIdentifier>{4FC737F1-C7A5-4376-A066-2A32D752A2FF}</UniqueIdentifier>
+      <Extensions>cpp;c;cc;cxx;c++;cppm;ixx;def;odl;idl;hpj;bat;asm;asmx</Extensions>
+    </Filter>
+    <Filter Include="Header Files">
+      <UniqueIdentifier>{93995380-89BD-4b04-88EB-625FBE52EBFB}</UniqueIdentifier>
+      <Extensions>h;hh;hpp;hxx;h++;hm;inl;inc;ipp;xsd</Extensions>
+    </Filter>
+    <Filter Include="Resource Files">
+      <UniqueIdentifier>{67DA6AB6-F800-4c08-8B7A-83BB121AAD01}</UniqueIdentifier>
+      <Extensions>rc;ico;cur;bmp;dlg;rc2;rct;bin;rgs;gif;jpg;jpeg;jpe;resx;tiff;tif;png;wav;mfcribbon-ms</Extensions>
+    </Filter>
+  </ItemGroup>
+  <ItemGroup>
+    <ClCompile Include="ExternalIR.cpp">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+  </ItemGroup>
+</Project>

commit b809e9c54832920dbb1eeeb080e488388e037ea5

fix(customir): copy all of the combo-exscore-rate graph data
Was missing multiplication of .size() by sizeof(int).

Fixes: 4ec48ab05db2 ("feat: custom IR API")

LR2/LR2_customir.cpp
@@ -614,10 +614,17 @@ IRScoreInternal::IRScoreInternal(game& game, sqlite3* sql, int _player) {
 
 	if (!courseScore) {
 		::GRAPHDATA& statgraph = gameplay.statgraph[_player];
+
 		graphs.hp = statgraph.hp;
-		memcpy(graphs.combo.data(), statgraph.combo, graphs.combo.size());
-		memcpy(graphs.exscore.data(), statgraph.exscore, graphs.exscore.size());
-		memcpy(graphs.rate.data(), gameplay.rategraph[_player].val, graphs.rate.size());
+
+		static_assert(sizeof(statgraph.combo) == sizeof(graphs.combo));
+		std::ranges::copy(statgraph.combo, graphs.combo.begin());
+
+		static_assert(sizeof(statgraph.exscore) == sizeof(graphs.exscore));
+		std::ranges::copy(statgraph.exscore, graphs.exscore.begin());
+
+		static_assert(sizeof(gameplay.rategraph[_player].val) == sizeof(graphs.rate));
+		std::ranges::copy(gameplay.rategraph[_player].val, graphs.rate.begin());
 	}
 
 	if (!courseScore && _player == 0) {