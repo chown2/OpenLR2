chown2/OpenLR2

commit fb56c59cd7d7b8cb0db2efbd38bf19c015d6d39f

feat: add unrandomizer

LR2/structure.h
@@ -1320,6 +1320,7 @@ struct gameplay {
 	int bgaMixer[10];
 	char isPreviewLoad;
 	int randomLayoutForDisplay[2];
+	unsigned int forceRandomLayout; // 0 = disabled
 	std::future<void> hThreadPreview;
 	int previewStatus; /* 1:start 2:loaded */
 	CSTR previewBMShash;

LR2/LR2_bmsload.cpp
@@ -1,6 +1,8 @@
 ﻿#include "LR2_bmsload.h"
 #include "Engine.h"
 #include "LR2_replay.h"
+#include "Unrandomizer_SeedMap5K.h"
+#include "Unrandomizer_SeedMap7K.h"
 #include "filesystem.h"
 
 #include <algorithm>
@@ -1870,11 +1872,25 @@ int ParseBmsFile(gameplay *gp, CSTR filename, AUDIO *aud, ConfigStruct* cfg, BMS
 	oldSpeedMultiplier = gp->freqSpeedMultiplier;
 
 	//TOFIX : seed is not putted into replaydata, when use ghostbattle. (retry puts seed) (see also ProcS_Play())
-	if (gp->randomseed) {
+	if (gp->randomseed != 0) {
 		ErrorLogFmtAdd("RANDSEEDを引き継ぎます\n");
 	}
 	else if (gp->replay.status != 2) {
-		gp->randomseed = GetRand(0x7ffe);
+		if (cfg->play.random[0] == 2 && gp->forceRandomLayout != 0) {
+			ErrorLogFmtAdd("Force random layout %d for keymode %d\n", gp->forceRandomLayout, gp->keymode);
+			switch (gp->keymode) {
+			case 5: gp->randomseed = GetSeed5K(gp->forceRandomLayout); break;
+			case 7: gp->randomseed = GetSeed7K(gp->forceRandomLayout); break;
+			default: ErrorLogAdd("Unsupported keymode for forcing random layout\n"); break;
+			}
+			if (gp->randomseed == 0xFFFF) {
+				ErrorLogAdd("Impossible random layout\n");
+				gp->randomseed = 0;
+			}
+		}
+		if (gp->randomseed == 0) {
+			gp->randomseed = GetRand(0x7ffe);
+		}
 		if (gp->replay.status == 1) {
 			AddReplayData(&gp->replay, 0, 200, (short)gp->randomseed);
 		}

LR2/Scene02_Songselect.cpp
@@ -1283,6 +1283,35 @@ int CmdSearch(game *g, CSTR *cmd, sqlite3 *sql) {
 			return 1;
 		}
 	}
+	else if (cmd->isSame("/random")) {
+		*cmd = g->gameplay.forceRandomLayout == 0 ? "OFF" : std::to_string(g->gameplay.forceRandomLayout).c_str();
+		return 1;
+	}
+	else if (cmd->starts_with("/random ")) {
+		auto s = std::string_view{ cmd->body }.substr(std::string_view{ "/random " }.length());
+		unsigned int val;
+		auto [ptr, ec] = std::from_chars(s.data(), s.data() + s.size(), val);
+		if (ec != std::errc()) {
+			*cmd = ("INVALID NUMBER: " + std::string(s)).c_str();
+			return 1;
+		}
+		if (val == 0) {
+			g->gameplay.forceRandomLayout = 0;
+			*cmd = g->gameplay.forceRandomLayout == 0 ? "OFF" : std::to_string(g->gameplay.forceRandomLayout).c_str();
+		}
+		else if (val >= 12345 && val <= 54321) {
+			g->gameplay.forceRandomLayout = val * 100 + 67;
+			*cmd = g->gameplay.forceRandomLayout == 0 ? "OFF" : std::to_string(g->gameplay.forceRandomLayout).c_str();
+		}
+		else if (val >= 1234567 && val <= 7654321) {
+			g->gameplay.forceRandomLayout = val;
+			*cmd = g->gameplay.forceRandomLayout == 0 ? "OFF" : std::to_string(g->gameplay.forceRandomLayout).c_str();
+		}
+		else {
+			*cmd = ("OUT OF RANGE NUMBER: " + std::to_string(val)).c_str();
+		}
+		return 1;
+	}
 	else {
 		*cmd = "COMMAND ERROR";
 		return 1;

LR2/Unrandomizer_SeedMap5K.h
@@ -1,0 +1,126 @@
+// \retval 0xFFFF Impossible layout
+inline int GetSeed5K(int in) {
+	switch (in) {
+	case 1234567: return 88;
+	case 1235467: return 13497;
+	case 1243567: return 18411;
+	case 1245367: return 26358;
+	case 1253467: return 23975;
+	case 1254367: return 27606;
+	case 1324567: return 5512;
+	case 1325467: return 10114;
+	case 1342567: return 6649;
+	case 1345267: return 7315;
+	case 1352467: return 2934;
+	case 1354267: return 20599;
+	case 1423567: return 15224;
+	case 1425367: return 92;
+	case 1432567: return 18668;
+	case 1435267: return 27553;
+	case 1452367: return 28053;
+	case 1453267: return 3376;
+	case 1523467: return 8087;
+	case 1524367: return 13889;
+	case 1532467: return 17537;
+	case 1534267: return 29290;
+	case 1542367: return 12328;
+	case 1543267: return 14394;
+	case 2134567: return 27773;
+	case 2135467: return 18256;
+	case 2143567: return 2477;
+	case 2145367: return 10537;
+	case 2153467: return 13573;
+	case 2154367: return 13686;
+	case 2314567: return 27979;
+	case 2315467: return 5751;
+	case 2341567: return 17730;
+	case 2345167: return 8183;
+	case 2351467: return 5989;
+	case 2354167: return 17293;
+	case 2413567: return 3588;
+	case 2415367: return 2412;
+	case 2431567: return 29918;
+	case 2435167: return 22372;
+	case 2451367: return 12169;
+	case 2453167: return 8773;
+	case 2513467: return 29374;
+	case 2514367: return 18079;
+	case 2531467: return 27660;
+	case 2534167: return 6115;
+	case 2541367: return 20163;
+	case 2543167: return 29855;
+	case 3124567: return 10836;
+	case 3125467: return 524;
+	case 3142567: return 1455;
+	case 3145267: return 26835;
+	case 3152467: return 8119;
+	case 3154267: return 21911;
+	case 3214567: return 10785;
+	case 3215467: return 19;
+	case 3241567: return 31137;
+	case 3245167: return 30223;
+	case 3251467: return 8691;
+	case 3254167: return 22494;
+	case 3412567: return 8203;
+	case 3415267: return 22234;
+	case 3421567: return 8625;
+	case 3425167: return 6237;
+	case 3451267: return 11168;
+	case 3452167: return 3559;
+	case 3512467: return 9470;
+	case 3514267: return 9034;
+	case 3521467: return 29114;
+	case 3524167: return 27983;
+	case 3541267: return 25448;
+	case 3542167: return 32428;
+	case 4123567: return 25119;
+	case 4125367: return 32085;
+	case 4132567: return 2993;
+	case 4135267: return 7124;
+	case 4152367: return 18710;
+	case 4153267: return 1756;
+	case 4213567: return 14115;
+	case 4215367: return 28436;
+	case 4231567: return 16128;
+	case 4235167: return 8044;
+	case 4251367: return 21665;
+	case 4253167: return 20653;
+	case 4312567: return 27441;
+	case 4315267: return 10538;
+	case 4321567: return 24744;
+	case 4325167: return 32316;
+	case 4351267: return 28254;
+	case 4352167: return 3016;
+	case 4512367: return 25980;
+	case 4513267: return 4249;
+	case 4521367: return 28520;
+	case 4523167: return 26504;
+	case 4531267: return 9282;
+	case 4532167: return 18033;
+	case 5123467: return 23233;
+	case 5124367: return 6401;
+	case 5132467: return 5272;
+	case 5134267: return 4092;
+	case 5142367: return 962;
+	case 5143267: return 31224;
+	case 5213467: return 10810;
+	case 5214367: return 1628;
+	case 5231467: return 9535;
+	case 5234167: return 3061;
+	case 5241367: return 3811;
+	case 5243167: return 13462;
+	case 5312467: return 28957;
+	case 5314267: return 18262;
+	case 5321467: return 2041;
+	case 5324167: return 27512;
+	case 5341267: return 11686;
+	case 5342167: return 24574;
+	case 5412367: return 17343;
+	case 5413267: return 9808;
+	case 5421367: return 17435;
+	case 5423167: return 3797;
+	case 5431267: return 15623;
+	case 5432167: return 12276;
+	default: return 0xFFFF;
+	}
+}

commit 6e6d0f493d6c998f49cbe733786ddfc45af7612f

fix(structure): init forceRandomLayout (2/2)
Fixes https://github.com/GOMazk/OpenLR2/issues/147 "Uninitialized new
fields in struct gameplay".

Fixes: fb56c59cd7d7 ("feat: add unrandomizer")

LR2/structure.h
@@ -1332,7 +1332,7 @@ struct gameplay {
 	int bgaMixer[10];
 	char isPreviewLoad;
 	int randomLayoutForDisplay[2]{};
-	unsigned int forceRandomLayout; // 0 = disabled
+	unsigned int forceRandomLayout{0}; // 0 = disabled
 	std::future<void> hThreadPreview;
 	int previewStatus; /* 1:start 2:loaded */
 	CSTR previewBMShash;