52286a74929e2cd00b67c501df33909999a3fbc5
GOMazk <gommazuk@naver.com>
2026-07-11 09:57:23 +0900

    Update version string for release

--- LR2/LR2_version.cpp
+++ LR2/LR2_version.cpp
@@ -2,6 +2,6 @@
 
 namespace openlr2 {
 const char* clientName = "OpenLR2";
-const char* versionName = "OpenLR2 version 260707";
-const int versionCode = 260707;
+const char* versionName = "OpenLR2 version 260710";
+const int versionCode = 260710;
 } // namespace openlr2
