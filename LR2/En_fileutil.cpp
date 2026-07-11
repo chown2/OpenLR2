chown2/OpenLR2

commit 0e4299c2e10766924b5c8df9011007a25737b109

perf(fileutil): use WinAPI for converting UTF-16 -> UTF-8
Like 10 times faster in debug builds, make the game start twice as fast.
Also, wstring_convert is removed in C++26. Double win.

LR2/En_fileutil.cpp
@@ -41,8 +41,15 @@ std::string ansi2utf(const std::string_view str, unsigned int codepage) {
 	int size_needed = MultiByteToWideChar(codepage, 0, str.data(), static_cast<int>(str.size()), nullptr, 0);
 	auto wstr = std::make_unique_for_overwrite<wchar_t[]>(size_needed);
 	MultiByteToWideChar(codepage, 0, str.data(), static_cast<int>(str.size()), wstr.get(), size_needed);
-	std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t> converter;
-	return converter.to_bytes(wstr.get(), wstr.get() + size_needed);
+
+	int narrow_buf_size = WideCharToMultiByte(CP_UTF8, 0, wstr.get(), size_needed, 0, 0, 0, FALSE);
+	auto lstr = std::make_unique_for_overwrite<char[]>(narrow_buf_size);
+	WideCharToMultiByte(CP_UTF8, 0, wstr.get(), narrow_buf_size, lstr.get(), narrow_buf_size, 0, FALSE);
+
+	std::string out;
+	// NOTE: narrow_buf_size doesn't include null-terminator as we are passing size to WideCharToMultiByte explicitly.
+	out.assign(lstr.get(), narrow_buf_size);
+	return out;
 }
 
 #else

commit 2a7b29922360bcffa3a802ced66edb76271fe028

fix(fileutil): use the correct buffer size when converting encodings
It should've been impossible for this to work, but it did.
This code was copied from Lunatic Vibes F where it also worked fine:
https://github.com/chown2/lunaticvibesf/issues/257.

Fixes: 0e4299c2e107 ("perf(fileutil): use WinAPI for converting UTF-16 -> UTF-8")

LR2/En_fileutil.cpp
@@ -38,13 +38,13 @@ std::string utf2ansi(const std::string_view in, unsigned int codepage) {
 }
 
 std::string ansi2utf(const std::string_view str, unsigned int codepage) {
-	int size_needed = MultiByteToWideChar(codepage, 0, str.data(), static_cast<int>(str.size()), nullptr, 0);
-	auto wstr = std::make_unique_for_overwrite<wchar_t[]>(size_needed);
-	MultiByteToWideChar(codepage, 0, str.data(), static_cast<int>(str.size()), wstr.get(), size_needed);
+	int wide_buf_size = MultiByteToWideChar(codepage, 0, str.data(), static_cast<int>(str.size()), nullptr, 0);
+	auto wstr = std::make_unique_for_overwrite<wchar_t[]>(wide_buf_size);
+	MultiByteToWideChar(codepage, 0, str.data(), static_cast<int>(str.size()), wstr.get(), wide_buf_size);
 
-	int narrow_buf_size = WideCharToMultiByte(CP_UTF8, 0, wstr.get(), size_needed, 0, 0, 0, FALSE);
+	int narrow_buf_size = WideCharToMultiByte(CP_UTF8, 0, wstr.get(), wide_buf_size, nullptr, 0, nullptr, nullptr);
 	auto lstr = std::make_unique_for_overwrite<char[]>(narrow_buf_size);
-	WideCharToMultiByte(CP_UTF8, 0, wstr.get(), narrow_buf_size, lstr.get(), narrow_buf_size, 0, FALSE);
+	WideCharToMultiByte(CP_UTF8, 0, wstr.get(), wide_buf_size, lstr.get(), narrow_buf_size, nullptr, nullptr);
 
 	std::string out;
 	// NOTE: narrow_buf_size doesn't include null-terminator as we are passing size to WideCharToMultiByte explicitly.