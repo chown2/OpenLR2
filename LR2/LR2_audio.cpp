chown2/OpenLR2

commit 5c255d0957194d16a3f927572a6f8ef4a2f5b42a

fix: UTF-8 strings part 2

LR2/LR2_audio.cpp
@@ -3,7 +3,7 @@
 #include "LR2_replay.h"
 #include "LR2_skinobject.h"
 #include "LR2_configsave.h"
-
+#include "filesystem.h"
 #include "filesystem.h"
 
 int FxByMIDI(game *g) {
@@ -345,12 +345,13 @@ int ReadLR2SoundSet(game *g, CSTR filepath, char reFlag) {
 		int t = sku.customize_value[i];
 		if (899 < t &&  t < 1000) dst_op[t] = 1;
 	}
-	hFile = _wfopen(utf2ws(filepath.body).c_str(), L"r");
+	hFile = fopen(filepath.body, "r");
 	if (!hFile) return 0;
 	CSTR fBuf(260);
 	char* pFbuf;
 	pFbuf = fBuf.outstr();
 	for (pFbuf = fgets(pFbuf, 256, hFile); pFbuf; pFbuf = fgets(pFbuf, 256, hFile)) {
+		fBuf = ansi2utf(pFbuf, 932).c_str();
 		if (*fBuf.atPos(0) == '#') {
 			fBuf.trimWhiteSpace();
 			DealWhiteSpace(&fBuf);

LR2/LR2_bmsload.cpp
@@ -2,7 +2,7 @@
 #include "Engine.h"
 #include "LR2_replay.h"
 #include "filesystem.h"
-
+#include "filesystem.h"
 #include <algorithm>
 #include <unordered_map>
 
@@ -2000,7 +2000,7 @@ int ParseBmsFile(gameplay *gp, CSTR filename, AUDIO *aud, ConfigStruct* cfg, BMS
 				ErrorLogFmtAdd("エキスパ:ステージ=%d、フルパス=%s\n", gp->courseStageNow, filename.body);
 			}
 		}
-		hFile = _wfopen(utf2ws(filename.body).c_str(), L"r");
+		hFile = fopen(filename.body, "r");
 		if (hFile == NULL) {
 			ErrorLogAdd("BMSを開けません。\n");
 			return -1;
@@ -2030,6 +2030,7 @@ int ParseBmsFile(gameplay *gp, CSTR filename, AUDIO *aud, ConfigStruct* cfg, BMS
 				*fBuf.atPos(0) = '\0';
 				continue;
 			}
+			fBuf = ansi2utf(pFbuf, 932).c_str();
 			fBufOrg = fBuf;
 			fBuf.upper();
 

LR2/LR2_skinload.cpp
@@ -3,7 +3,7 @@
 #include "LR2_configsave.h"
 #include "En_fileutil.h"
 #include "filesystem.h"
-
+#include "filesystem.h"
 bool IsMultibyte(byte ch){
 	/*if (0x80 < ch) {
 		if (ch < 0xa0) return true;
@@ -938,7 +938,7 @@ int ReadSkin(skstruct *sk,CSTR FilePath, int unused, int skin_num, SkinUser* sku
 	ErrorLogFmtAdd("スキンの読み込みを開始します。 %s\n", FilePath.body);
 	ErrorLogTabAdd();
 
-	pFile = _wfopen(utf2ws(FilePath.body).c_str(), L"r");
+	pFile = fopen(FilePath.body, "r");
 	CSTR dir(FilePath.getDirectory());
 	line = 0;
 
@@ -949,6 +949,7 @@ int ReadSkin(skstruct *sk,CSTR FilePath, int unused, int skin_num, SkinUser* sku
 	}
 	pFbuf = fBuf.outstr();
 	for (pFbuf = fgets(pFbuf, 1023, pFile); pFbuf; pFbuf = fgets(pFbuf, 1023, pFile)) {
+		fBuf = ansi2utf(pFbuf, 932).c_str();
 		//if (sk->image.dstSize - 1 == sk->image.srcSize) {
 		//	sk->image.src = (SRCstruct*)realloc(sk->image.src, (sk->image.dstSize + 50) * sizeof(SRCstruct));
 		//	sk->image.dst = (DSTstruct*)realloc(sk->image.dst, (sk->image.dstSize + 50) * sizeof(DSTstruct));

LR2/LR2_skinmanage.cpp
@@ -1,8 +1,9 @@
 ﻿#include "LR2_skinmanage.h"
+#include "En_fileutil.h"
 #include "Engine.h"
 #include "LR2_configsave.h"
 #include "filesystem.h"
-
+#include "filesystem.h"
 #include <filesystem>
 
 //maybe deleted by compiler, restored it for convenience
@@ -222,13 +223,14 @@ int ParseLR2SkinCustom(SkinManage *skm, CSTR filepath) {
 
 	cstrSprintf(&md5Filepath, fs::make_preferred("LR2files/SkinCustomize/%s.xml").data(), MD5str(filepath) );
 	ReadSkinCustomize(&skCustom, md5Filepath);
-	pFile = _wfopen(utf2ws(filepath.body).c_str(), L"r");
+	pFile = fopen(filepath.body, "r");
 	if (!pFile) return 0;
 
 	pBuffer = buffer.outstr();
 	for (pBuffer = fgets(pBuffer,256,pFile) ; pBuffer ; pBuffer = fgets(pBuffer, 256, pFile)) {
 		buffer.trimWhiteSpace();
 		DealWhiteSpace(&buffer);
+		buffer = ansi2utf(pBuffer, 932).c_str();
 		if (buffer.left(12).isSame("#INFORMATION")) {
 			SplitCSV(buffer, &csvBuf, ",");
 			if (csvBuf.val[1] < 0) {
@@ -381,30 +383,6 @@ int ParseLR2SkinCustom(SkinManage *skm, CSTR filepath) {
 int MakeSkinList(SkinManage *skm, CSTR dir) {
 	if (dir.right(1).isDiff("\\") && dir.right(1).isDiff("/"))
 		dir.add("\\");
-#ifdef _WIN32
-	CSTR filter;
-	filter.assign(&dir).add("*");
-	HANDLE hFindFile, hFindFileOld;
-	_WIN32_FIND_DATAA findFileData;
-	hFindFile = FindFirstFileA(filter, &findFileData);
-	do{
-		if ( (findFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) == 0) {
-			filter.assign(&dir).add(findFileData.cFileName);
-			if (filter.right(8).isSame(".lr2skin") || filter.right(6).isSame(".lr2ss")) ParseLR2SkinCustom(skm, filter);
-		}
-		else {  //logic arranged
-		if (strcmp("..", findFileData.cFileName) && strcmp(".", findFileData.cFileName)) {
-			filter.assign(&dir).add(findFileData.cFileName).add("\\");
-			MakeSkinList(skm, filter);
-		}
-		}
-		hFindFileOld = hFindFile;
-		if (FindNextFileA(hFindFile, &findFileData) == 0) {
-			FindClose(hFindFileOld);
-			return 0;
-		}
-	} while (true);
-#else // TODO(utf-8): use this
 #ifndef _WIN32
 	dir.replace("\\", "/");
 #endif // _WIN32
@@ -420,5 +398,4 @@ int MakeSkinList(SkinManage *skm, CSTR dir) {
 		}
 	}
 	return 0;
-#endif // _WIN32
 }

LR2/LR2_skinobject.cpp
@@ -7,7 +7,7 @@
 #include "LR2_statplay.h"
 #include "Scenes.h"
 #include "filesystem.h"
-
+#include "filesystem.h"
 #include <DxLib/DxLib.h>
 
 #ifndef _WIN32
@@ -4255,7 +4255,7 @@ int ReadOptionstrFile(OptionString *arrOpStr, CSTR filepath) {
 	CSVbuf csv;
 
 	DefineOptionStrNum(arrOpStr);
-	pFile = _wfopen(utf2ws(filepath.body).c_str(), L"r");
+	pFile = fopen(filepath.body, "r");
 	if (pFile == 0) {
 		ErrorLogAdd("オプション文字列リストが見つかりません。\n");
 		return 0;
@@ -4263,6 +4263,7 @@ int ReadOptionstrFile(OptionString *arrOpStr, CSTR filepath) {
 
 	pFbuf = fBuf.outstr();
 	for (pFbuf = fgets(pFbuf, bufSize, pFile); pFbuf; pFbuf = fgets(pFbuf, bufSize, pFile)) {
+		fBuf = ansi2utf(pFbuf, 932).c_str();
 		if (*(fBuf.atPos(0)) == '#') {
 			SplitCSV(fBuf, &csv, ",");
 

LR2/Scene02_Songselect.cpp
@@ -3,7 +3,6 @@
 #include "LR2.h"
 #include <stdio.h>
 #include "filesystem.h"
-
 #ifndef _WIN32
 #include "En_dxlibstub.h"
 #endif // _WIN32
@@ -650,7 +649,7 @@ int ShowReadmes(game *g) {
 		g->txtStruct.readme.path = g->txtStruct.readme.folderpath;
 		g->txtStruct.readme.path.add(filename.c_str());
 
-		_wfopen_s(&pFile, utf2ws(g->txtStruct.readme.path.body).c_str(), L"r");
+		pFile = fopen(g->txtStruct.readme.path.body, "r");
 
 		char *pFbuf = fBuf.outstr();
 		if (pFile != NULL) {
@@ -713,17 +712,14 @@ int ShowReadme(game *g, CSTR path) {
 
 	CSTR fBuf(0x400);
 
-#ifdef _WIN32
-	_wfopen_s(&pFile, utf2ws(g->txtStruct.readme.path.body).c_str(), L"r");
-#else
 	pFile = fopen(g->txtStruct.readme.path, "r");
-#endif // _WIN32
 
 	char *pFbuf = fBuf.outstr();
 	if (pFile != NULL) {
 		g->txtStruct.readme.show = 1;
 		pFbuf = fBuf.outstr();
 		for (pFbuf = fgets(pFbuf, 0x3FC, pFile); pFbuf; pFbuf = fgets(pFbuf, 0x3FC, pFile)) {
+			fBuf = ansi2utf(pFbuf, 932).c_str();
 			g->txtStruct.readme.body[g->txtStruct.readme.lines] = fBuf;
 			g->txtStruct.readme.lines++;
 			int len = GetTextGraphLength(&fBuf, &g->skstruct.ImageFonts[g->skstruct.src_README[0].cycle]);

LR2/Scene07_Skinselect.cpp
@@ -1,17 +1,10 @@
 #include "Scene07_Skinselect.h"
 #include "LR2.h"
 #include "Scenes.h"
-
+#include "filesystem.h"
 #include "filesystem.h"
 
 int SkinSelect_SoundSet(game *g, CSTR filepath) {
-	
-	FILE *pFile;
-
-	CSTR fBuf(1024);
-	char* pFbuf;
-	CSVbuf csv;
-
 	StopSysSound(g);
 	ReleaseSound(&g->audio, &g->audio.sysSound.select);
 	ReleaseSound(&g->audio, &g->audio.sysSound.folder_open);
@@ -27,25 +20,30 @@ int SkinSelect_SoundSet(game *g, CSTR filepath) {
 	ReleaseSound(&g->audio, &g->audio.sysSound.exselect);
 	if (g->audio.is_fmod_disabled == 0) FMOD_System_Update(g->audio.fmodSys);
 	
-	pFile = _wfopen(utf2ws(filepath.body).c_str(), L"r");
+	FILE* pFile = fopen(filepath.body, "r");
 	if (pFile == 0) return 0;
 
-	pFbuf = fBuf.outstr();
-	for (pFbuf = fgets(pFbuf, 1023, pFile); pFbuf; pFbuf = fgets(pFbuf, 1023, pFile)) {
-		if (*fBuf.atPos(0) = '#') {
-			fBuf.trimWhiteSpace();
-			DealWhiteSpace(&fBuf);
-			if (isdigit(*fBuf.atPos(1)) && isdigit(*fBuf.atPos(2))) {
-				
-				int type = atol(fBuf.getSliced(1, 2));
-				if (type < 0) continue;
+	CSVbuf csv;
 
-				SplitCSV(fBuf, &csv, ",");
-				g->config.skin.skinFilePath[type] = csv.str[1];
-				SetFirstSkin_5k(&g->skinData, (SKINTYPE)type, &g->config.skin.skinFilePath[type]);
-			}
-			*fBuf.atPos(0) = 0;
+	CSTR fBuf(1024);
+	char* pFbuf = fBuf.outstr();
+	for (pFbuf = fgets(pFbuf, 1023, pFile); pFbuf; pFbuf = fgets(pFbuf, 1023, pFile)) {
+		if (*fBuf.atPos(0) != '#') {
+			continue;
 		}
+		fBuf.trimWhiteSpace();
+		DealWhiteSpace(&fBuf);
+		fBuf = ansi2utf(pFbuf, 932).c_str();
+		if (isdigit(*fBuf.atPos(1)) && isdigit(*fBuf.atPos(2))) {
+
+			int type = atol(fBuf.getSliced(1, 2));
+			if (type < 0) continue;
+
+			SplitCSV(fBuf, &csv, ",");
+			g->config.skin.skinFilePath[type] = csv.str[1];
+			SetFirstSkin_5k(&g->skinData, (SKINTYPE)type, &g->config.skin.skinFilePath[type]);
+		}
+		*fBuf.atPos(0) = 0;
 	}
 
 	fclose(pFile);

CMakeLists.txt
@@ -97,16 +97,17 @@ if(WIN32)
     target_link_libraries(OpenLR2Lib PUBLIC winmm.lib)
 endif()
 
-if(${CMAKE_CXX_COMPILER_ID} STREQUAL "MSVC")
-    # FIXME(utf-8): not supported on any other compilers. Move to UTF-8.
-    target_compile_options(OpenLR2Lib PUBLIC /execution-charset:shift-JIS)
+if(WIN32)
+    # FIXME: only do this for msvc, `${CMAKE_CXX_COMPILER_ID} STREQUAL "MSVC"` doesn't work
+    target_compile_options(OpenLR2Lib PUBLIC /utf-8)
 endif()
 
-add_executable(OpenLR2 WIN32 LR2/Main.cpp)
+add_executable(OpenLR2 WIN32 LR2/Main.cpp OpenLR2.manifest)
 set_target_properties(OpenLR2 PROPERTIES CXX_STANDARD 23)
 target_link_libraries(OpenLR2 PUBLIC OpenLR2Lib)
 
 if(WIN32)
+    # FIXME: SkinEditor manifest
     add_executable(
         SkinEditor
         WIN32

commit d64533baebbf458ac673983a3c41db05234ac08b

fix: reset pointers to buffers after potential invalidation by ansi2utf
Fixes 50/50 crashes on Remi-S DP loading.
It has CSV files with 16384 commas on each line. Baffling.

Fixes: 5c255d095719 ("fix: UTF-8 strings part 2")

LR2/LR2_audio.cpp
@@ -352,6 +352,7 @@ int ReadLR2SoundSet(game *g, CSTR filepath, char reFlag) {
 	pFbuf = fBuf.outstr();
 	for (pFbuf = fgets(pFbuf, 256, hFile); pFbuf; pFbuf = fgets(pFbuf, 256, hFile)) {
 		fBuf = ansi2utf(pFbuf, 932).c_str();
+		pFbuf = fBuf.outstr();
 		if (*fBuf.atPos(0) == '#') {
 			fBuf.trimWhiteSpace();
 			DealWhiteSpace(&fBuf);

LR2/LR2_bmsload.cpp
@@ -2251,6 +2251,7 @@ int ParseBmsFile(gameplay *gp, CSTR filename, AUDIO *aud, ConfigStruct* cfg, BMS
 				continue;
 			}
 			fBuf = ansi2utf(pFbuf, 932).c_str();
+			pFbuf = fBuf.outstr();
 			fBufOrg = fBuf;
 			fBuf.upper();
 

LR2/LR2_skinload.cpp
@@ -877,6 +877,7 @@ int ReadSkin(skstruct *sk,CSTR FilePath, int unused, int skin_num, SkinUser* sku
 	pFbuf = fBuf.outstr();
 	for (pFbuf = fgets(pFbuf, 1023, pFile); pFbuf; pFbuf = fgets(pFbuf, 1023, pFile)) {
 		fBuf = ansi2utf(pFbuf, 932).c_str();
+		pFbuf = fBuf.outstr();
 		ExpandSkinObjectMax(&sk->image, 50);
 		for (auto& obj : sk->otherObject) {
 			ExpandSkinObjectMax(&obj, 20);

LR2/LR2_skinmanage.cpp
@@ -231,6 +231,7 @@ int ParseLR2SkinCustom(SkinManage *skm, CSTR filepath) {
 		buffer.trimWhiteSpace();
 		DealWhiteSpace(&buffer);
 		buffer = ansi2utf(pBuffer, 932).c_str();
+		pBuffer = buffer.outstr();
 		if (buffer.left(12).isSame("#INFORMATION")) {
 			SplitCSV(buffer, &csvBuf, ",");
 			if (csvBuf.val[1] < 0) {

LR2/LR2_skinobject.cpp
@@ -4311,6 +4311,7 @@ int ReadOptionstrFile(TextStruct& txtStruct, CSTR filepath) {
 	pFbuf = fBuf.outstr();
 	for (pFbuf = fgets(pFbuf, bufSize, pFile); pFbuf; pFbuf = fgets(pFbuf, bufSize, pFile)) {
 		fBuf = ansi2utf(pFbuf, 932).c_str();
+		pFbuf = fBuf.outstr();
 		if (*(fBuf.atPos(0)) == '#') {
 			SplitCSV(fBuf, &csv, ",");
 

LR2/Scene02_Songselect.cpp
@@ -652,6 +652,7 @@ int ShowReadmes(game *g) {
 		char* pFbuf = fBuf.outstr();
 		for (pFbuf = fgets(pFbuf, 2048, pFile); pFbuf; pFbuf = fgets(pFbuf, 2048, pFile)) {
 			fBuf = ansi2utf(fBuf.body, 932).c_str();
+			pFbuf = fBuf.outstr();
 			DealWhiteSpace(&fBuf);
 			g->txtStruct.readme.body[g->txtStruct.readme.lines] = fBuf;
 			g->txtStruct.readme.lines++;
@@ -698,6 +699,7 @@ int ShowReadme(game *g, CSTR path) {
 		pFbuf = fBuf.outstr();
 		for (pFbuf = fgets(pFbuf, 0x3FC, pFile); pFbuf; pFbuf = fgets(pFbuf, 0x3FC, pFile)) {
 			fBuf = ansi2utf(pFbuf, 932).c_str();
+			pFbuf = fBuf.outstr();
 			g->txtStruct.readme.body[g->txtStruct.readme.lines] = fBuf;
 			g->txtStruct.readme.lines++;
 			int len = GetTextGraphLength(&fBuf, &g->skstruct.ImageFonts[g->skstruct.src_README[0].cycle]);

LR2/Scene07_Skinselect.cpp
@@ -34,6 +34,7 @@ int SkinSelect_SoundSet(game *g, CSTR filepath) {
 		fBuf.trimWhiteSpace();
 		DealWhiteSpace(&fBuf);
 		fBuf = ansi2utf(pFbuf, 932).c_str();
+		pFbuf = fBuf.outstr();
 		if (isdigit(*fBuf.atPos(1)) && isdigit(*fBuf.atPos(2))) {
 
 			int type = atol(fBuf.getSliced(1, 2));