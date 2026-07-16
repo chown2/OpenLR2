chown2/OpenLR2

commit 1ed630fb02e3f3b10ed8d63e3a3a364a2a334622

refactor: use OPTION_GAUGE enum for missing hardcoded value
And rename GaugeOption to GaugeType, and fix typo of 'PATTCK'

LR2/Scene05_Result.cpp
@@ -225,24 +225,24 @@ static bool fWaitHiScoreUpdateInput = false;
 int ProcI_Result(game *g) {
 
 	auto switch_gauge_display = [](gameplay& gameplay, int buttonVal, PLAYERSTATUS& player) {
-		if (buttonVal == 1 && player.gaugeType != 5) {
+		if (buttonVal == 1 && player.gaugeType != OPTION_GAUGE_PATTACK) {
 			int& gauge = player.gaugeType;
 			if (gameplay.courseType != 2) [[likely]] {
 				switch (gauge) {
-				case 0: gauge = 1; break;
-				case 1: gauge = 2; break;
-				case 2: gauge = 4; break;
-				case 3: gauge = 0; break;
-				case 4: gauge = 3; break;
+				case OPTION_GAUGE_EASY: gauge = OPTION_GAUGE_GROOVE; break;
+				case OPTION_GAUGE_GROOVE: gauge = OPTION_GAUGE_HARD; break;
+				case OPTION_GAUGE_HARD: gauge = OPTION_GAUGE_DEATH; break;
+				case OPTION_GAUGE_DEATH: gauge = OPTION_GAUGE_PATTACK; break;
+				case OPTION_GAUGE_PATTACK: gauge = OPTION_GAUGE_EASY; break;
 				default: break;
 				}
 			}
 			else {
 				switch (gauge) {
-				case 0: gauge = 1; break;
-				case 1: gauge = 2; break;
-				case 2: gauge = 4; break;
-				case 4: gauge = 0; break;
+				case OPTION_GAUGE_GROOVE: gauge = OPTION_GAUGE_HARD; break;
+				case OPTION_GAUGE_HARD: gauge = OPTION_GAUGE_DEATH; break;
+				case OPTION_GAUGE_DEATH: gauge = OPTION_GAUGE_PATTACK; break;
+				case OPTION_GAUGE_PATTACK: gauge = OPTION_GAUGE_GROOVE; break;
 				default: break;
 				}
 			}

LR2/LR2_bmsload.cpp
@@ -556,7 +556,7 @@ int InitGameplay(gameplay *gp, CONFIG_PLAY *cfg) {
 		std::array<EXTENDEDPLAYERSTATS, 20> extendedColumnStatsCourse = gp->player[p].extendedColumnStatsCourse;
 
 		gp->player[p] = PLAYERSTATUS();
-		gp->player[p].gaugeType = cfg->gaugeOption[p];
+		gp->player[p].gaugeType = cfg->gaugeType[p];
 		gp->statgraph[p] = GRAPHDATA();
 
 		if (gp->courseStageNow > 0) {
@@ -840,7 +840,7 @@ int InitGameplay_retry(gameplay *gp, AUDIO *snd, game *g) {
 		auto tempDmg = gp->player[p].judge_damage;
 		memcpy(tempTime, &gp->player[p].judgetime, sizeof(tempTime));
 		gp->player[p] = PLAYERSTATUS();
-		gp->player[p].gaugeType = g->config.play.gaugeOption[p];
+		gp->player[p].gaugeType = g->config.play.gaugeType[p];
 		gp->player[p].judge_damage = tempDmg;
 		memcpy(&gp->player[p].judgetime, tempTime, sizeof(tempTime));
 		gp->player[p].totalnotes = tempCount;

LR2/LR2_configsave.cpp
@@ -214,7 +214,7 @@ int WriteConfigXml(game *g, const char *filename){
 	sprintf(buf, "\t\t<%s>%d</%s>\n", "shuttermargin", (g->config).play.shuttermargin, "shuttermargin");
 	fputs(buf, pFile);
 
-	sprintf(buf, "\t\t<%s>%d</%s>\n", "gauge", (g->config).play.gaugeOption[PLAYER_1], "gauge");
+	sprintf(buf, "\t\t<%s>%d</%s>\n", "gauge", (g->config).play.gaugeType[PLAYER_1], "gauge");
 	fputs(buf, pFile);
 
 	sprintf(buf, "\t\t<%s>%d</%s>\n", "random", (g->config).play.random[PLAYER_1], "random");
@@ -1079,7 +1079,7 @@ int ReadConfig(game* g, const char* filepath) {
 
 	ReadXml_Int("config", "play", "hs", 100, &g->config.play.hiSpeed[PLAYER_1], hXml);
 	g->config.play.hiSpeed[PLAYER_2] = g->config.play.hiSpeed[PLAYER_1];
-	ReadXml_Int("config", "play", "gauge", OPTION_GAUGE_GROOVE, g->config.play.gaugeOption, hXml);
+	ReadXml_Int("config", "play", "gauge", OPTION_GAUGE_GROOVE, g->config.play.gaugeType, hXml);
 	ReadXml_Int("config", "play", "random", OPTION_RANDOM_OFF, &g->config.play.random[PLAYER_1], hXml);
 	ReadXml_Int("config", "play", "effect", 0, &g->config.play.m_HIDSUD[PLAYER_1], hXml);
 	ReadXml_Int("config", "play", "hstype", OPTION_HSFIX_OFF, &g->config.play.hsfix, hXml);

LR2/LR2_customir.cpp
@@ -325,7 +325,7 @@ struct IRScoreInternal {
 		int courseType{};
 	} song{};
 	struct SETTINGS {
-		int gaugeOption{};
+		int gaugeType{};
 		std::array<int, PLAYER_MAX> random{};
 		bool autokey{};
 		std::array<bool, PLAYER_MAX> assist{};
@@ -424,7 +424,7 @@ void IRScoreInternal::MakeScoreV1(IRScoreV1& scoreOut) const {
 	scoreOut.song.courseStageCount = song.courseStageCount;
 	scoreOut.song.courseType = song.courseType;
 
-	scoreOut.settings.gaugeOption = settings.gaugeOption;
+	scoreOut.settings.gaugeOption = settings.gaugeType;
 	for (int p : { PLAYER_1, PLAYER_2})
 		scoreOut.settings.random[p] = settings.random[p];
 	scoreOut.settings.autokey = (int)settings.autokey;
@@ -546,7 +546,7 @@ IRScoreInternal::IRScoreInternal(game& game, sqlite3* sql, int _player, std::str
 		songPlayLevel = curSong.level;
 	}
 	CONFIG_PLAY& cfg = game.config.play;
-	settings.gaugeOption = cfg.gaugeOption[_player];
+	settings.gaugeType = cfg.gaugeType[_player];
 	settings.random[PLAYER_1] = cfg.random[PLAYER_1];
 	settings.random[PLAYER_2] = cfg.random[PLAYER_2];
 	settings.autokey = cfg.autokey;

LR2/LR2_ir.cpp
@@ -718,7 +718,7 @@ bool NETWORK::GetTargetInfo(int mode, CSTR songmd5, CSTR *oData, CSTR *oName, in
 		case openlr2::Gauge::Survival: *oDigit1 = OPTION_GAUGE_HARD; break;
 		case openlr2::Gauge::Death: *oDigit1 = OPTION_GAUGE_DEATH; break;
 		case openlr2::Gauge::Easy: *oDigit1 = OPTION_GAUGE_EASY; break;
-		case openlr2::Gauge::PAttack: *oDigit1 = OPTION_GAUGE_PATTCK; break;
+		case openlr2::Gauge::PAttack: *oDigit1 = OPTION_GAUGE_PATTACK; break;
 		case openlr2::Gauge::GAttack: *oDigit1 = OPTION_GAUGE_GATTACK; break;
 		default: *oDigit1 = 0; break;
 		}

LR2/LR2_replay.cpp
@@ -192,8 +192,8 @@ int AddReplayDataHeader(CONFIG_PLAY *cfg, REPLAY *rp, AUDIO *snd, gameplay *gp){
 	AddReplayData(rp, 0, 0x99, static_cast<short>(cfg->random[PLAYER_2]));
 	AddReplayData(rp, 0, 0x68, static_cast<short>(cfg->m_HIDSUD[PLAYER_1]));
 	AddReplayData(rp, 0, 0x9a, static_cast<short>(cfg->m_HIDSUD[PLAYER_2]));
-	AddReplayData(rp, 0, 0x65, static_cast<short>(cfg->gaugeOption[PLAYER_1]));
-	AddReplayData(rp, 0, 0x97, static_cast<short>(cfg->gaugeOption[PLAYER_2]));
+	AddReplayData(rp, 0, 0x65, static_cast<short>(cfg->gaugeType[PLAYER_1]));
+	AddReplayData(rp, 0, 0x97, static_cast<short>(cfg->gaugeType[PLAYER_2]));
 	AddReplayData(rp, 0, 0x66, static_cast<short>(cfg->lanecoverv[PLAYER_1]));
 	AddReplayData(rp, 0, 0x98, static_cast<short>(cfg->lanecoverv[PLAYER_2]));
 	AddReplayData(rp, 0, 0x6b, static_cast<short>(cfg->assist[PLAYER_1]));
@@ -638,7 +638,7 @@ int ReplayDataToInput(ReplayData *data, game *g, AUDIO *aud, gameplay *gp, input
 			g->config.play.hiSpeed[PLAYER_1] = data->value;
 			break;
 		case 0x65:
-			g->config.play.gaugeOption[PLAYER_1] = data->value;
+			g->config.play.gaugeType[PLAYER_1] = data->value;
 			break;
 		case 0x66:
 			g->config.play.lanecoverv[PLAYER_1] = data->value;
@@ -662,7 +662,7 @@ int ReplayDataToInput(ReplayData *data, game *g, AUDIO *aud, gameplay *gp, input
 			g->config.play.hiSpeed[PLAYER_2] = data->value;
 			break;
 		case 0x97:
-			g->config.play.gaugeOption[PLAYER_2] = data->value;
+			g->config.play.gaugeType[PLAYER_2] = data->value;
 			break;
 		case 0x98:
 			g->config.play.lanecoverv[PLAYER_2] = data->value;

LR2/LR2_skinobject.cpp
@@ -151,19 +151,19 @@ bool GetOptionFlag_dst(game *gs, int option) {
 			break;
 
 		case 42:
-			if (gs->gameplay.player[PLAYER_1].gaugeType != 0 && gs->gameplay.player[PLAYER_1].gaugeType != 3) return !ret;
+			if (gs->gameplay.player[PLAYER_1].gaugeType != OPTION_GAUGE_GROOVE && gs->gameplay.player[PLAYER_1].gaugeType != OPTION_GAUGE_EASY) return !ret;
 			if (gs->gameplay.isCourse == 0) return ret;
 			break;
 		case 43:
-			if (gs->gameplay.player[PLAYER_1].gaugeType != 0 && gs->gameplay.player[PLAYER_1].gaugeType != 3) return ret;
+			if (gs->gameplay.player[PLAYER_1].gaugeType != OPTION_GAUGE_GROOVE && gs->gameplay.player[PLAYER_1].gaugeType != OPTION_GAUGE_EASY) return ret;
 			if (gs->gameplay.isCourse == 0) return !ret;
 			return ret;
 		case 44:
-			if (gs->gameplay.player[PLAYER_2].gaugeType != 0 && gs->gameplay.player[PLAYER_2].gaugeType != 3) return !ret;
+			if (gs->gameplay.player[PLAYER_2].gaugeType != OPTION_GAUGE_GROOVE && gs->gameplay.player[PLAYER_2].gaugeType != OPTION_GAUGE_EASY) return !ret;
 			if (gs->gameplay.isCourse == 0) return ret;
 			break;
 		case 45:
-			if (gs->gameplay.player[PLAYER_2].gaugeType != 0 && gs->gameplay.player[PLAYER_2].gaugeType != 3) return ret;
+			if (gs->gameplay.player[PLAYER_2].gaugeType != OPTION_GAUGE_GROOVE && gs->gameplay.player[PLAYER_2].gaugeType != OPTION_GAUGE_EASY) return ret;
 			if (gs->gameplay.isCourse == 0) return !ret;
 			return ret;
 
@@ -3255,7 +3255,7 @@ int SetObjectValue_Button(game *g, skstruct *sk, Timer *T, char flag) {
 			case 40:
 				isClickSuccess = g->procSelecter == 4 || g->procSelecter == 5 || g->procSelecter == 13 ?
 					ButtonByInput(&sk->drBuf, &sk->otherObject[1].src[i], &sk->otherObject[1].dst[i], T, &g->KeyInput, &g->gameplay.player[PLAYER_1].gaugeType, OPTION_GAUGE_GROOVE, OPTION_GAUGE_END, g->sSelect.panel) :
-					ButtonByInput(&sk->drBuf, &sk->otherObject[1].src[i], &sk->otherObject[1].dst[i], T, &g->KeyInput, &g->config.play.gaugeOption[PLAYER_1], OPTION_GAUGE_GROOVE, OPTION_GAUGE_END, g->sSelect.panel);
+					ButtonByInput(&sk->drBuf, &sk->otherObject[1].src[i], &sk->otherObject[1].dst[i], T, &g->KeyInput, &g->config.play.gaugeType[PLAYER_1], OPTION_GAUGE_GROOVE, OPTION_GAUGE_END, g->sSelect.panel);
 				if (isClickSuccess == 2) {
 					PlaySound(&g->audio, &g->audio.sysSound.option_change, g->audio.chnKey, -1);
 					SetObjectStrings_SongSelect(g);
@@ -3266,7 +3266,7 @@ int SetObjectValue_Button(game *g, skstruct *sk, Timer *T, char flag) {
 				if (g->config.play.battle == OPTION_BATTLE_BATTLE) {
 					isClickSuccess = g->procSelecter == 4 || g->procSelecter == 5 || g->procSelecter == 13 ?
 						ButtonByInput(&sk->drBuf, &sk->otherObject[1].src[i], &sk->otherObject[1].dst[i], T, &g->KeyInput, &g->gameplay.player[PLAYER_2].gaugeType, OPTION_GAUGE_GROOVE, OPTION_GAUGE_END, g->sSelect.panel) :
-						ButtonByInput(&sk->drBuf, &sk->otherObject[1].src[i], &sk->otherObject[1].dst[i], T, &g->KeyInput, &g->config.play.gaugeOption[PLAYER_2], OPTION_GAUGE_GROOVE, OPTION_GAUGE_END, g->sSelect.panel);
+						ButtonByInput(&sk->drBuf, &sk->otherObject[1].src[i], &sk->otherObject[1].dst[i], T, &g->KeyInput, &g->config.play.gaugeType[PLAYER_2], OPTION_GAUGE_GROOVE, OPTION_GAUGE_END, g->sSelect.panel);
 					if (isClickSuccess == 2) {
 						PlaySound(&g->audio, &g->audio.sysSound.option_change, g->audio.chnKey, -1);
 						SetObjectStrings_SongSelect(g);

LR2/LR2_statplay.cpp
@@ -29,11 +29,11 @@ int CheckClearLampChallenge(game *g){ //TOFIX : assist[PLAYER_2] == 1 but no bat
 		&& g->config.play.assist[PLAYER_1] == 0 && (g->config.play.assist[PLAYER_2] == 0 || g->sSelect.metaSelected.keymode < 10)) {
 		switch (g->procSelecter == 4 || g->procSelecter == 5 || g->procSelecter == 13
 			? g->gameplay.player[PLAYER_1].gaugeType
-			: g->config.play.gaugeOption[PLAYER_1]) {
+			: g->config.play.gaugeType[PLAYER_1]) {
 		case OPTION_GAUGE_HARD: return 3;
 		case OPTION_GAUGE_DEATH: return 4;
 		case OPTION_GAUGE_EASY: return 1;
-		case OPTION_GAUGE_PATTCK: return 4;
+		case OPTION_GAUGE_PATTACK: return 4;
 		case OPTION_GAUGE_GATTACK: return 3;
 		default: return 2;
 		}
@@ -152,7 +152,7 @@ int CheckClear(PLAYERSTATUS *pstat, int gaugeType, char isCourse){
 		pstat->clearType = 5;
 		return pstat->clearType;
 	}
-	if (gaugeType == 1 || gaugeType == 5 || gaugeType == 4) {
+	if (gaugeType == OPTION_GAUGE_HARD || gaugeType == OPTION_GAUGE_GATTACK || gaugeType == OPTION_GAUGE_PATTACK) {
 		if ( pstat->note_current == pstat->totalnotes && pstat->HP[gaugeType] >= 2.0) {
 			pstat->clearType = 4;
 		}
@@ -162,12 +162,12 @@ int CheckClear(PLAYERSTATUS *pstat, int gaugeType, char isCourse){
 			if (pstat->note_current != pstat->totalnotes)
 				return pstat->clearType;
 			if (pstat->HP[gaugeType] >= 2.0) {
-				pstat->clearType = (gaugeType != 3) + 2;
+				pstat->clearType = (gaugeType != OPTION_GAUGE_EASY) + 2;
 				return pstat->clearType;
 			}
 		}
 		if (pstat->note_current == pstat->totalnotes && pstat->HP[gaugeType] >= 80.0) {
-			pstat->clearType = (gaugeType != 3) + 2;
+			pstat->clearType = (gaugeType != OPTION_GAUGE_EASY) + 2;
 			return pstat->clearType;
 		}
 	}
@@ -200,8 +200,8 @@ static int GetBestClearedGauge(const gameplay& gameplay, int playerIdx, const CO
 	const PLAYERSTATUS& player = gameplay.player[playerIdx];
 	if (gameplay.isAutoplay) return player.gaugeType;
 	if (playerIdx == PLAYER_2 && gameplay.ghostBattle) return player.gaugeType;
-	if (cfg.gaugeOption[playerIdx] == 5) return 5;
-	constexpr std::array<int, 5> gaugeArr({ 4, 2, 1, 0, 3 });
+	if (cfg.gaugeType[playerIdx] == OPTION_GAUGE_GATTACK) return OPTION_GAUGE_GATTACK;
+	constexpr std::array<int, 5> gaugeArr({ OPTION_GAUGE_PATTACK, OPTION_GAUGE_DEATH, OPTION_GAUGE_HARD, OPTION_GAUGE_GROOVE, OPTION_GAUGE_EASY });
 	unsigned int i = 0;
 	if (limitToCourse)
 		for (; i < gaugeArr.size(); i++)
@@ -209,8 +209,8 @@ static int GetBestClearedGauge(const gameplay& gameplay, int playerIdx, const CO
 				break;
 	auto is_gauge_alive = [](int gaugeIdx, double hp) {
 		switch (gaugeIdx) {
-		case 0:
-		case 3:
+		case OPTION_GAUGE_GROOVE:
+		case OPTION_GAUGE_EASY:
 			return hp >= 80.;
 		default: return hp >= 2.;
 		}
@@ -218,8 +218,8 @@ static int GetBestClearedGauge(const gameplay& gameplay, int playerIdx, const CO
 	for (; i < gaugeArr.size(); i++)
 		if (is_gauge_alive(gaugeArr[i], player.HP[gaugeArr[i]]))
 			return gaugeArr[i];
-	if (gameplay.courseType == 2) return 0;
-	return 3;
+	if (gameplay.courseType == 2) return OPTION_GAUGE_GROOVE;
+	return OPTION_GAUGE_EASY;
 }
 
 int CheckCourseClear(game* g) {
@@ -246,7 +246,7 @@ int CheckCourseClear(game* g) {
 		else if (g->gameplay.player[p].total_note == g->gameplay.player[p].max_combo_course) {
 			g->gameplay.player[p].clearType = 5;
 		}
-		else if (gauge[p] == 1 || gauge[p] == 5 || gauge[p] == 4) {
+		else if (gauge[p] == OPTION_GAUGE_HARD || gauge[p] == OPTION_GAUGE_GATTACK || gauge[p] == OPTION_GAUGE_PATTACK) {
 			if (g->gameplay.player[p].note_current2 == g->gameplay.player[p].total_note && g->gameplay.player[p].HP[gauge[p]] > 2.0) {
 				g->gameplay.player[p].clearType = 4;
 			}
@@ -273,9 +273,9 @@ int CheckMission(game *g){
 
 	gauge = g->gameplay.player[PLAYER_1].gaugeType;
 
-	if (g->gameplay.player[PLAYER_1].gaugeType == 3)
+	if (g->gameplay.player[PLAYER_1].gaugeType == OPTION_GAUGE_EASY)
 		return 0;
-	if (g->gameplay.player[PLAYER_2].gaugeType == 3)
+	if (g->gameplay.player[PLAYER_2].gaugeType == OPTION_GAUGE_EASY)
 		return 0;
 
 	//converge 7 14 25 35 40

commit 9a22c1782c8b6ff4955cc2b69f09c8649b58525d

fix(result): fix typo in GAS gauge switch condition blocking it
Fixes: 1ed630fb02e3 ("refactor: use OPTION_GAUGE enum for missing hardcoded value")

LR2/Scene05_Result.cpp
@@ -226,7 +226,7 @@ static bool fWaitHiScoreUpdateInput = false;
 int ProcI_Result(game *g) {
 
 	auto switch_gauge_display = [](gameplay& gameplay, int buttonVal, PLAYERSTATUS& player) {
-		if (buttonVal == 1 && player.gaugeType != OPTION_GAUGE_PATTACK) {
+		if (buttonVal == 1 && player.gaugeType != OPTION_GAUGE_GATTACK) {
 			int& gauge = player.gaugeType;
 			if (gameplay.courseType != 2) [[likely]] {
 				switch (gauge) {